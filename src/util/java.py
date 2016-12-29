f8ca485cceaf2056d893910e700cac62c6fe6d11
Oliver Lade <oliver.lade@anz.com>
2016-12-29 16:03:23 +1100

    Fix Python build encoding errors

--- src/util/java.py
+++ src/util/java.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 # Copyright 2016 The Sysl Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License."""Super smart code writer."""
-
-# -*- encoding: utf-8 -*-
 
 import contextlib
 import decimal
