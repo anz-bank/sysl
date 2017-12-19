6413dfbc55901f342569eb33c99e46efdf87075f
Julia Ogris <julia.ogris@gmail.com>
2017-12-20 10:28:27 +1100

    :rotating_light: Integrate flake8 linting in travis CI with setuptools

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ import sys
 from shutil import rmtree
 
 from setuptools import find_packages, setup, Command
-from setuptools.command.test import test as TestCommand
 
 # Package meta-data.
 NAME = 'sysl'
@@ -36,54 +35,6 @@ about = {}
 with open(os.path.join(here, 'src', NAME, '__version__.py')) as f:
     exec(f.read(), about)
 
-
-class CleanCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system('rm -vrf ./build ./dist ./.eggs ./*.pyc ./*.tgz ./*.egg-info')
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ''
-
-    def run_tests(self):
-        import shlex
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
-
-
-class DistCommand(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-        os.system(
-            '{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-        sys.exit()
-
-
 setup(
     name=NAME,
     version=about['__version__'],
@@ -105,11 +56,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
     ],
-    setup_requires=['pytest-runner', 'pytest'],
+    setup_requires=['pytest-runner', 'pytest', 'flake8'],
     tests_require=['pytest'],
-    cmdclass={
-        'clean': CleanCommand,
-        'test': PyTest,
-        'dist': DistCommand,
-    }
 )
