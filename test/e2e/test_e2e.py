f0554453361d24f279ac950955becfbfc6430313
Julia Ogris <julia.ogris@gmail.com>
2018-01-16 10:50:18 +1100

    :ok_hand: Improve <repo>/tmp directory handling

--- test/e2e/test_e2e.py
+++ test/e2e/test_e2e.py
@@ -2,9 +2,12 @@ from sysl.core import syslloader
 from sysl.util.file import filesAreIdentical
 
 import pytest
+import os
 
 from os import path, remove
 from subprocess import call
+
+REPO_ROOT = path.normpath(path.join(path.dirname(__file__), '..', '..'))
 
 
 def remove_file(fname):
@@ -22,16 +25,18 @@ def remove_file(fname):
     '005_annotations',
 ])
 def test_e2e(fname):
-    dname = path.dirname(__file__)
-    root = path.join(dname, 'input')
+    e2e_dir = path.normpath(path.dirname(__file__))
+    e2e_rel_dir = os.path.relpath(e2e_dir, start=REPO_ROOT)
+
+    root = path.join(e2e_dir, 'input')
     model = '/' + fname
-    out_fname = path.join(dname, '..', '..', 'tmp',
-                          'test', 'e2e', 'actual_output', fname + '.txt')
+    out_dir = path.join(REPO_ROOT, 'tmp', e2e_rel_dir)
+    out_fname = path.join(out_dir, 'actual_output', fname + '.txt')
     remove_file(out_fname)
 
     cmd = ['sysl', '--root', root, 'textpb', '-o', out_fname, model]
     print 'calling', ' '.join(cmd)
     call(cmd)
 
-    expected_fname = path.join(dname, 'expected_output', fname + '.txt')
+    expected_fname = path.join(e2e_dir, 'expected_output', fname + '.txt')
     assert filesAreIdentical(expected_fname, out_fname)
