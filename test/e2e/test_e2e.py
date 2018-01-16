3a8c453a2e319b26a3e64777943a47055ab52122
Julia Ogris <julia.ogris@gmail.com>
2018-01-16 13:41:38 +1100

    Move e2e output to <repo>/tmp (#33)
    Ideally no generated files would be located in the source tree, but that can be difficult cross-platform so put them all in the tmp top-level directory.

--- test/e2e/test_e2e.py
+++ test/e2e/test_e2e.py
@@ -6,6 +6,8 @@ import pytest
 from os import path, remove
 from subprocess import call
 
+REPO_ROOT = path.normpath(path.join(path.dirname(__file__), '..', '..'))
+
 
 def remove_file(fname):
     try:
@@ -22,15 +24,18 @@ def remove_file(fname):
     '005_annotations',
 ])
 def test_e2e(fname):
-    dname = path.dirname(__file__)
-    root = path.join(dname, 'input')
+    e2e_dir = path.normpath(path.dirname(__file__))
+    e2e_rel_dir = path.relpath(e2e_dir, start=REPO_ROOT)
+
+    root = path.join(e2e_dir, 'input')
     model = '/' + fname
-    out_fname = path.join(dname, 'actual_output', fname + '.txt')
+    out_dir = path.join(REPO_ROOT, 'tmp', e2e_rel_dir)
+    out_fname = path.join(out_dir, 'actual_output', fname + '.txt')
     remove_file(out_fname)
 
     cmd = ['sysl', '--root', root, 'textpb', '-o', out_fname, model]
     print 'calling', ' '.join(cmd)
     call(cmd)
 
-    expected_fname = path.join(dname, 'expected_output', fname + '.txt')
+    expected_fname = path.join(e2e_dir, 'expected_output', fname + '.txt')
     assert filesAreIdentical(expected_fname, out_fname)
