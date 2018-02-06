01726ef69210f98c2729cf96d38025c0cf35ff18
Julia Ogris <julia.ogris@gmail.com>
2018-02-07 08:29:54 +1100

    Remove string testing for JSON comparison

--- test/java/src/test/java/TestJsonSerializer.java
+++ test/java/src/test/java/TestJsonSerializer.java
@@ -15,15 +15,16 @@ import org.joda.time.LocalDate;
 import io.sysl.reljam.gen.tuple.complex.BuyItemFormComplex;
 import io.sysl.reljam.gen.tuple.complex.CustomerType;
 import io.sysl.reljam.gen.tuple.complex.Email;
+import io.sysl.reljam.gen.tuple.complex.Address;
 import io.sysl.reljam.gen.tuple.complex.UserFormComplexJsonDeserializer;
 import io.sysl.reljam.gen.tuple.complex.UserFormComplexJsonSerializer;
 
 public class TestJsonSerializer
 {
     @Test
-    public void testSerializeDeserialize() throws IOException {
-        BuyItemFormComplex e1 = loadBuyItemFormComplex("src/test/resources/test_1.json");
-        BuyItemFormComplex e2 = createBuyItemFormComplex_1();
+    public void testJson1() throws IOException {
+        BuyItemFormComplex e1 = loadBuyItemFormComplex("src/test/resources/test1.json");
+        BuyItemFormComplex e2 = createBuyItemFormComplex1();
         assertEquals(e1, e2);
 
         String s1 = serialize(e1);
@@ -32,10 +33,14 @@ public class TestJsonSerializer
     }
 
     @Test
-    public void testSubObject()  throws IOException {
-        BuyItemFormComplex entity = loadBuyItemFormComplex("src/test/resources/test_2.json");
-        String str = serialize(entity);
-        assertTrue(str.equals(TEST_2));
+    public void testJson2()  throws IOException {
+        BuyItemFormComplex e1 = loadBuyItemFormComplex("src/test/resources/test2.json");
+        BuyItemFormComplex e2 = createBuyItemFormComplex2();
+        assertEquals(e1, e2);
+
+        String s1 = serialize(e1);
+        String s2 = serialize(e2);
+        assertEquals(s1, s2);
     }
 
     public static String serialize(BuyItemFormComplex entity) throws IOException {
@@ -49,7 +54,7 @@ public class TestJsonSerializer
         return w.toString();
     }
 
-    public static BuyItemFormComplex createBuyItemFormComplex_1() {
+    public static BuyItemFormComplex createBuyItemFormComplex1() {
         Email one = new Email();
         one.setEmail(new String("john.smith@anz.com"));
         Email two = new Email();
@@ -74,6 +79,24 @@ public class TestJsonSerializer
         return e;
     }
 
+    public static BuyItemFormComplex createBuyItemFormComplex2() {
+        BuyItemFormComplex e = createBuyItemFormComplex1();
+        e.setDateOfBirth(null);
+        e.setTags(null);
+
+        Address a = new Address();
+        a.setAddress1(new String("ANZ Building"));
+        a.setAddress2(new String("833 Collins Street"));
+        a.setCity(new String("Melbourne"));
+        a.setState(new String("VIC"));
+        a.setZipCode(new String("3000"));
+        a.setCountry(new String("AU"));
+
+        e.setBillingAddress(a);
+
+        return e;
+    }
+
     public static BuyItemFormComplex loadBuyItemFormComplex(String filename) throws IOException {
         JsonFactory factory = new JsonFactory();
         JsonParser p = factory.createParser(new File(filename));
@@ -82,24 +105,4 @@ public class TestJsonSerializer
         return ser.deserialize(p, (BuyItemFormComplex)null);
     }
 
-    public static String LS = System.getProperty("line.separator");
-    public static String TEST_2 =   "{" + LS
-                                    + "  \"Amount\" : 10.11," + LS
-                                    + "  \"BillingAddress\" : {" + LS
-                                    + "    \"Address1\" : \"ANZ Building\"," + LS
-                                    + "    \"Address2\" : \"833 Collins Street\"," + LS
-                                    + "    \"City\" : \"Melbourne\"," + LS
-                                    + "    \"Country\" : \"AU\"," + LS
-                                    + "    \"State\" : \"VIC\"," + LS
-                                    + "    \"ZipCode\" : \"3000\"" + LS
-                                    + "  }," + LS
-                                    + "  \"CustomerType\" : 0," + LS
-                                    + "  \"Emails\" : [ {" + LS
-                                    + "    \"Email\" : \"jsmith@anz.com\"" + LS
-                                    + "  }, {" + LS
-                                    + "    \"Email\" : \"john.smith@anz.com\"" + LS
-                                    + "  } ]," + LS
-                                    + "  \"FirstName\" : \"John\"," + LS
-                                    + "  \"LastName\" : \"Smith\"" + LS
-                                    + "}";
 }
