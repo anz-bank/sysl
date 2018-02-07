8e8ce5e921c41b4dfcb7f18af423fd404df61a26
Julia Ogris <julia.ogris@gmail.com>
2018-02-07 14:00:55 +1100

    Fix linter warnings and code review updates

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -4,71 +4,71 @@ USAGE="Usage: release.sh <prepare|deploy> MAJOR.MINOR.PATCH"
 COMMAND=$1
 VERSION=$2
 
-function exit_error {
+function fatal {
     echo "$@" >&2
     exit 1
 }
 if [ "$#" -ne 2 ]; then
-    exit_error $USAGE
+    fatal "$USAGE"
 fi
 if ! [[ $VERSION =~ ^[[:digit:]+\\.[:digit:]+\\.[:digit:]]+$ ]]; then
-	exit_error $USAGE
+	fatal "$USAGE"
 fi
 if [[ $COMMAND != "prepare" && $COMMAND != "deploy" ]]; then
-	exit_error $USAGE
+	fatal "$USAGE"
 fi
 if [[ $(git status --porcelain 2> /dev/null | tail -n1) != "" ]]; then
-	exit_error "Repo is not clean please commit or delete dirty files."
+	fatal "Repo is not clean please commit or delete dirty files."
 fi
 
-ORIGIN_URL=`git remote get-url origin`
-ORIGIN=`echo "$ORIGIN_URL" | sed -n "s/.*\/\/github.com\/\(..*\)\/..*.git/\1/p"`
+ORIGIN_URL=$(git remote get-url origin)
+ORIGIN=$(echo "$ORIGIN_URL" | sed -n "s/.*\\/\\/github.com\\/\\(..*\\)\\/..*.git/\\1/p")
 UPSTREAM="anz-bank"
-UPSTREAM_URL=`echo "$ORIGIN_URL" | sed -n "s/\(.*\/\/github.com\/\)\(..*\)\(\/..*.git\)/\1$UPSTREAM\3/p"`
+UPSTREAM_URL=$(echo "$ORIGIN_URL" | sed -n "s/\\(.*\\/\\/github.com\\/\\)\\(..*\\)\\(\\/..*.git\\)/\\1$UPSTREAM\\3/p")
 RELEASE_BRANCH="release-v$VERSION"
 
 echo "------- Checkout master ---------"
-git checkout master || exit_error "Cannot checkout master"
+git checkout master || fatal "Cannot checkout master"
 
 echo "------- Pull upstream ---------"
-git pull $UPSTREAM_URL master || exit_error "Cannot pull  upstream master"
+git pull "$UPSTREAM_URL" master || fatal "Cannot pull  upstream master"
 
 echo "------- Create release branch $RELEASE_BRANCH ---------"
-git co -b $RELEASE_BRANCH || exit_error "Cannot create release branch $RELEASE_BRANCH"
+git co -b "$RELEASE_BRANCH" || fatal "Cannot create release branch $RELEASE_BRANCH"
 
 echo "------- Update version ---------"
 echo "__version__ = '$VERSION'" > 'src/sysl/__version__.py' || { echo "Could not override src/sysl/__version__.py"; exit 1; }
 
 echo "------- Commit ---------"
-git commit -am "Bump version to $VERSION" || exit_error "Cannot commit version update"
+git commit -am "Bump version to $VERSION" || fatal "Cannot commit version update"
 
 echo "------- Push ---------"
-git push -u origin || exit_error "Cannot push release branch to origin"
+git push -u origin || fatal "Cannot push release branch to origin"
 
 echo "------- Create PR ---------"
 REPO="sysl"
-CREDENTIALS=`echo "host=github.com" | git credential fill`
-USERNAME=`echo $CREDENTIALS | sed -n "s/.*username=\([^ ]*\).*/\1/p"`
-PASSWORD=`echo $CREDENTIALS | sed -n "s/.*password=\([^ ]*\).*/\1/p"`
+CREDENTIALS=$(echo "host=github.com" | git credential fill)
+USERNAME=$(echo "$CREDENTIALS" | sed -n "s/.*username=\\([^ ]*\\).*/\\1/p")
+PASSWORD=$(echo "$CREDENTIALS" | sed -n "s/.*password=\\([^ ]*\\).*/\\1/p")
 if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ]; then
-    exit_error "Could not get GitHub credentials, please create pull request manually."
+    fatal "Could not get GitHub credentials, please create pull request manually."
 fi
 
-JSON=`jq -nc "{title:\"Bump version to $VERSION\",head:\"$ORIGIN:$RELEASE_BRANCH\",base:\"master\"}"`
-RESPONSE=`wget --quiet --output-document=- --content-on-error \
+JSON="{title:\"Bump version to $VERSION\",head:\"$ORIGIN:$RELEASE_BRANCH\",base:\"master\"}"
+RESPONSE=$(wget --quiet --output-document=- --content-on-error \
                --user="$USERNAME" --password="$PASSWORD" --auth-no-challenge \
                --header="Content-Type: application/json" \
                --header="Accept: application/vnd.github.v3+json" \
-               --post-data="$JSON" "https://api.github.com/repos/$UPSTREAM/$REPO/pulls"`
+               --post-data="$JSON" "https://api.github.com/repos/$UPSTREAM/$REPO/pulls")
 
 WGET_STATUS=$?
 if [ $WGET_STATUS -eq 0 ]; then
-	GITHUB_PR_URL=`echo "$RESPONSE" | jq -r '.html_url'`
+	  GITHUB_PR_URL=$(echo "$RESPONSE" | jq -r '.html_url')
     echo "Pull request opened:"
     echo "$GITHUB_PR_URL"
     open "$GITHUB_PR_URL"
 elif [ $WGET_STATUS -eq 6 ]; then
-	exit_error "Wrong username or password/token."
+	  fatal "Wrong username or password/token"
 else
-    exit_error "Unknown error."
+    fatal "Unknown error"
 fi
