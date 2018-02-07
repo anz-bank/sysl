1964364fa6f3625c29027dd37abd30082b5e1beb
Julia Ogris <julia.ogris@gmail.com>
2018-02-07 15:12:05 +1100

    Add deploy command to release script

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -22,10 +22,8 @@ if [[ $(git status --porcelain 2> /dev/null | tail -n1) != "" ]]; then
 fi
 
 ORIGIN_URL=$(git remote get-url origin)
-ORIGIN=$(echo "$ORIGIN_URL" | sed -n "s/.*\\/\\/github.com\\/\\(..*\\)\\/..*.git/\\1/p")
 UPSTREAM="anz-bank"
 UPSTREAM_URL=$(echo "$ORIGIN_URL" | sed -n "s/\\(.*\\/\\/github.com\\/\\)\\(..*\\)\\(\\/..*.git\\)/\\1$UPSTREAM\\3/p")
-RELEASE_BRANCH="release-v$VERSION"
 
 echo "------- Checkout master ---------"
 git checkout master || fatal "Cannot checkout master"
@@ -33,41 +31,53 @@ git checkout master || fatal "Cannot checkout master"
 echo "------- Pull upstream ---------"
 git pull "$UPSTREAM_URL" master || fatal "Cannot pull  upstream master"
 
-echo "------- Create release branch $RELEASE_BRANCH ---------"
-git co -b "$RELEASE_BRANCH" || fatal "Cannot create release branch $RELEASE_BRANCH"
+if [[ $COMANND = "prepare" ]]; then
+    RELEASE_BRANCH="release-v$VERSION"
+    ORIGIN=$(echo "$ORIGIN_URL" | sed -n "s/.*\\/\\/github.com\\/\\(..*\\)\\/..*.git/\\1/p")
+    REPO=$(echo "$ORIGIN_URL" | sed -n "s/.*\\/\\/github.com\\/..*\\/\\(..*\\).git/\\1/p")
+    echo "------- Create release branch $RELEASE_BRANCH ---------"
+    git co -b "$RELEASE_BRANCH" || fatal "Cannot create release branch $RELEASE_BRANCH"
 
-echo "------- Update version and commit ---------"
-echo "__version__ = '$VERSION'" > 'src/sysl/__version__.py' || fatal "Could not override src/sysl/__version__.py"
-git commit -am "Bump version to $VERSION" || fatal "Cannot commit version update"
+    echo "------- Update version and commit ---------"
+    echo "__version__ = '$VERSION'" > 'src/sysl/__version__.py' || fatal "Could not override src/sysl/__version__.py"
+    git commit -am "Bump version to $VERSION" || fatal "Cannot commit version update"
 
-echo "------- Push ---------"
-git push -u origin || fatal "Cannot push release branch to origin"
+    echo "------- Push ---------"
+    git push -u origin || fatal "Cannot push release branch to origin"
 
-echo "------- Create PR ---------"
-REPO="sysl"
-CREDENTIALS=$(echo "host=github.com" | git credential fill)
-USERNAME=$(echo "$CREDENTIALS" | sed -n "s/.*username=\\([^ ]*\\).*/\\1/p")
-PASSWORD=$(echo "$CREDENTIALS" | sed -n "s/.*password=\\([^ ]*\\).*/\\1/p")
-if [ "$USERNAME" = "" ] || [ "$PASSWORD" = "" ]; then
-    fatal "Could not get GitHub credentials, please create pull request manually."
-fi
+    echo "------- Create PR ---------"
+    CREDENTIALS=$(echo "host=github.com" | git credential fill)
+    USERNAME=$(echo "$CREDENTIALS" | sed -n "s/.*username=\\([^ ]*\\).*/\\1/p")
+    PASSWORD=$(echo "$CREDENTIALS" | sed -n "s/.*password=\\([^ ]*\\).*/\\1/p")
+    if [ "$USERNAME" = "" ] || [ "$PASSWORD" = "" ]; then
+        fatal "Could not get GitHub credentials, please create pull request manually."
+    fi
 
-JSON="{\"title\":\"Bump version to $VERSION\",\"head\":\"$ORIGIN:$RELEASE_BRANCH\",\"base\":\"master\"}"
-RESPONSE=$(wget --quiet --output-document=- --content-on-error \
-               --user="$USERNAME" --password="$PASSWORD" --auth-no-challenge \
-               --header="Content-Type: application/json" \
-               --header="Accept: application/vnd.github.v3+json" \
-               --post-data="$JSON" \
-               "https://api.github.com/repos/$UPSTREAM/$REPO/pulls")
+    JSON="{\"title\":\"Bump version to $VERSION\",\"head\":\"$ORIGIN:$RELEASE_BRANCH\",\"base\":\"master\"}"
+    RESPONSE=$(wget --quiet --output-document=- --content-on-error \
+                   --user="$USERNAME" --password="$PASSWORD" --auth-no-challenge \
+                   --header="Content-Type: application/json" \
+                   --header="Accept: application/vnd.github.v3+json" \
+                   --post-data="$JSON" \
+                   "https://api.github.com/repos/$UPSTREAM/$REPO/pulls")
 
-WGET_STATUS=$?
-if [ $WGET_STATUS -eq 0 ]; then
-    GITHUB_PR_URL=$(echo "$RESPONSE" | jq -r '.html_url')
-    echo "Pull request opened:"
-    echo "$GITHUB_PR_URL"
-    open "$GITHUB_PR_URL"
-elif [ $WGET_STATUS -eq 6 ]; then
-    fatal "Wrong username or password/token"
-else
-    fatal "Unknown error"
+    WGET_STATUS=$?
+    if [ $WGET_STATUS -eq 0 ]; then
+        GITHUB_PR_URL=$(echo "$RESPONSE" | jq -r '.html_url')
+        echo "Pull request opened:"
+        echo "$GITHUB_PR_URL"
+        open "$GITHUB_PR_URL"
+    elif [ $WGET_STATUS -eq 6 ]; then
+        fatal "Wrong username or password/token"
+    else
+        fatal "Unknown error"
+    fi
+elif [[ $COMANND = "prepare" ]]; then
+    PY_VERSION=$(sed -n "s/__version__ = '\\([^ ]*\\)'/\\1/p" src/sysl/__version__.py)
+    [[  "$VERSION" =  "$PY_VERSION" ]] || fatal "Version ($VERSION) different from __version__.py ($PY_VERSION)"
+    echo "------- Tag release ---------"
+    git tag "v$VERSION" || fatal "Cannot create tag v$VERSION"
+    echo "------- Push tag ---------"
+    git push --tags "$UPSTREAM_URL"
+    echo "------- Deployment triggered on Travis and Appveyor ---------"
 fi
