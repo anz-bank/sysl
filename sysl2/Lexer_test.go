4f3f456aaa4f4a2a392a6f639bd4e074bcc8cc4c
Singh, Sahejpreet <sahejpreet.singh@anz.com>
2018-03-21 14:52:37 +1100

    Implement EBNF parser

--- sysl2/Lexer_test.go
+++ sysl2/Lexer_test.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-	"testing"
+    "testing"
 )
 
 func TestLexer(t *testing.T) {
-	content := `keywords:
+    content := `keywords:
         while | return |
         if | else | import`
 
-	regexes := make([]string, 7)
-	regexes[0] = `[|]`
-	regexes[1] = `[:]`
-	regexes[2] = `[+]`
-	regexes[3] = `[*]`
-	regexes[4] = `[?]`
-	regexes[5] = `import`
-	regexes[6] = `[a-z][a-z0-9]*`
+    regexes := make([]string, 7)
+    regexes[0] = `[|]`
+    regexes[1] = `[:]`
+    regexes[2] = `[+]`
+    regexes[3] = `[*]`
+    regexes[4] = `[?]`
+    regexes[5] = `import`
+    regexes[6] = `[a-z][a-z0-9]*`
 
-	tokens := []token{{6, "keywords"}, {1, ":"}, {6, "while"}, {0, "|"}, {6, "return"}, {0, "|"}, {6, "if"}, {0, "|"}, {6, "else"}, {0, "|"}, {5, "import"}, {-1, ""}}
+    tokens := []token{{6, "keywords"}, {1, ":"}, {6, "while"}, {0, "|"}, {6, "return"}, {0, "|"}, {6, "if"}, {0, "|"}, {6, "else"}, {0, "|"}, {5, "import"}, {-1, ""}}
 
-	lex := makeLexer(content, regexes)
-	for i, expected := range tokens {
-		tok := lex.nextToken()
-		if tok != expected {
-			t.Errorf("wrong token @ %d: expected %+v but got %+v", i, expected, tok)
-		}
-	}
+    lex := makeLexer(content, regexes)
+    for i, expected := range tokens {
+        tok := lex.nextToken()
+        if tok != expected {
+            t.Errorf("wrong token @ %d: expected %+v but got %+v", i, expected, tok)
+        }
+    }
 }
--- sysl2/lexer.go
+++ sysl2/lexer.go
@@ -2,23 +2,23 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+    "regexp"
 	"sort"
 
 	"github.com/anz-bank/sysl/sysl2/proto"
 )
 
 type token struct {
-	id   int
-	text string
+    id   int
+    text string
 }
 
 type lexer struct {
-	currentIndex        int
-	regexs              []*regexp.Regexp
-	content             string
+    currentIndex        int
+    regexs              []*regexp.Regexp
+    content             string
 	WS               *regexp.Regexp
-	ignoreWhiteSpace    bool
+    ignoreWhiteSpace    bool
 }
 
 var arr []string
@@ -85,48 +85,48 @@ func getTerminals(rules map[string]*sysl.Rule) []string {
 
 // Regular whitespace delimited
 func makeLexer(content string, regexs []string) lexer {
-	compiledRegexes := make([]*regexp.Regexp, len(regexs))
+    compiledRegexes := make([]*regexp.Regexp, len(regexs))
 
-	for i, reg := range regexs {
-		compiledRegex := regexp.MustCompile(reg)
-		compiledRegexes[i] = compiledRegex
-	}
+    for i, reg := range regexs {
+        compiledRegex := regexp.MustCompile(reg)
+        compiledRegexes[i] = compiledRegex
+    }
 
-	return lexer{
-		ignoreWhiteSpace:    true,
-		regexs:              compiledRegexes,
-		currentIndex:        0,
-		content:             content,
+    return lexer{
+        ignoreWhiteSpace:    true,
+        regexs:              compiledRegexes,
+        currentIndex:        0,
+        content:             content,
 		WS:               regexp.MustCompile(`[^ \t\r\n]+`),
-	}
+    }
 }
 
 func (l *lexer) nextToken() token {
-	longestMatchIndex := -1
-	var tokenString string
-	if l.currentIndex < len(l.content) {
-		longestMatchLength := 0
+    longestMatchIndex := -1
+    var tokenString string
+    if l.currentIndex < len(l.content) {
+        longestMatchLength := 0
 		locWS := l.WS.FindStringIndex(l.content[l.currentIndex:])
-		if locWS != nil {
-			start := l.currentIndex + locWS[0]
-			end := l.currentIndex + locWS[1]
-			str := l.content[start:end]
+        if locWS != nil {
+            start := l.currentIndex + locWS[0]
+            end := l.currentIndex + locWS[1]
+            str := l.content[start:end]
 
-			for i, r := range l.regexs {
-				loc := r.FindStringIndex(str)
-				if loc != nil {
-					matchLen := loc[1] - loc[0]
-					if loc[0] == 0 && matchLen > longestMatchLength {
-						longestMatchLength = loc[1]
-						longestMatchIndex = i
-						tokenString = str[loc[0]:loc[1]]
-					}
-				}
-			}
-			if longestMatchIndex != -1 {
-				l.currentIndex = (start + longestMatchLength)
-			}
-		}
-	}
-	return token{longestMatchIndex, tokenString}
+            for i, r := range l.regexs {
+                loc := r.FindStringIndex(str)
+                if loc != nil {
+                    matchLen := loc[1] - loc[0]
+                    if loc[0] == 0 && matchLen > longestMatchLength {
+                        longestMatchLength = loc[1]
+                        longestMatchIndex = i
+                        tokenString = str[loc[0]:loc[1]]
+                    }
+                }
+            }
+            if longestMatchIndex != -1 {
+                l.currentIndex = (start + longestMatchLength)
+            }
+        }
+    }
+    return token{longestMatchIndex, tokenString}
 }
