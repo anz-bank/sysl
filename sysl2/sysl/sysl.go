979fc8c0a6410b0d9ba422b6fe3f67aa3caf6722
Marcelo Cantos <marcelo.cantos@anz.com>
2019-01-06 22:14:35 +1100

    :white_check_mark: coverage++

--- sysl2/sysl/sysl.go
+++ sysl2/sysl/sysl.go
@@ -19,11 +19,6 @@ import (
 	"github.com/golang/protobuf/proto"
 )
 
-var (
-	root   = flag.String("root", ".", "sysl root directory for input files (default: .)")
-	output = flag.String("o", "", "output file name")
-)
-
 const (
 	// ParserSuccess is returned by parser when it was able to parse input correctly
 	ParserSuccess = 0
@@ -46,20 +41,25 @@ func (e exit) Error() string {
 	return e.message
 }
 
-func init() {
-	flag.Parse()
+// JSONPB ...
+func JSONPB(m *sysl.Module, filename string) error {
+	if m == nil {
+		return fmt.Errorf("module is nil: %#v", filename)
+	}
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	return FJSONPB(f, m)
 }
 
-// JsonPB ...
-func JsonPB(m *sysl.Module, filename string) bool {
-	ma := jsonpb.Marshaler{Indent: " ", EmitDefaults: false}
-	f, err := os.Create(filename)
-	err = ma.Marshal(f, m)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return false
+// FJSONPB ...
+func FJSONPB(w io.Writer, m *sysl.Module) error {
+	if m == nil {
+		return fmt.Errorf("module is nil")
 	}
-	return true
+	ma := jsonpb.Marshaler{Indent: " ", EmitDefaults: false}
+	return ma.Marshal(w, m)
 }
 
 // TextPB ...
@@ -627,25 +627,71 @@ func FSParse(filename string, fs http.FileSystem) (*sysl.Module, error) {
 	return listener.module, nil
 }
 
-func main() {
-	fmt.Printf("Args: %v\n", flag.Args())
-	fmt.Printf("Root: %s\n", *root)
-	fmt.Printf("Module: %s\n", flag.Arg(0))
+// main3 is the real main function. It takes its output streams and command-line
+// arguments as parameters to support testability.
+func main3(stdout, stderr io.Writer, args []string) error {
+	flags := flag.NewFlagSet(args[0], flag.PanicOnError)
+
+	root := flags.String("root", ".", "sysl root directory for input files (default: .)")
+	output := flags.String("o", "", "output file name")
+	mode := flags.String("mode", "textpb", "output mode")
+
+	flags.Parse(args[1:])
+
+	switch *mode {
+	case "", "textpb", "json":
+	default:
+		return fmt.Errorf("Invalid -mode %#v", *mode)
+	}
+
+	filename := flags.Arg(0)
+
+	fmt.Fprintf(stderr, "Args: %v\n", flags.Args())
+	fmt.Fprintf(stderr, "Root: %s\n", *root)
+	fmt.Fprintf(stderr, "Module: %s\n", filename)
+	fmt.Fprintf(stderr, "Mode: %s\n", *mode)
 	format := strings.ToLower(*output)
-	toJson := strings.HasSuffix(format, ".json")
-	mod, err := Parse(flag.Arg(0), *root)
+	toJSON := *mode == "json" || *mode == "" && strings.HasSuffix(format, ".json")
+	fmt.Fprintf(stderr, "%s\n", filename)
+	mod, err := Parse(filename, *root)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		if err, ok := err.(exit); ok {
-			os.Exit(err.code)
-		}
-		os.Exit(1)
+		return err
 	}
 	if mod != nil {
-		if toJson {
-			JsonPB(mod, *output)
-		} else {
-			TextPB(mod, *output)
+		if toJSON {
+			if *output == "-" {
+				return FJSONPB(stdout, mod)
+			}
+			return JSONPB(mod, *output)
 		}
+		if *output == "-" {
+			return FTextPB(stdout, mod)
+		}
+		return TextPB(mod, *output)
+	}
+	return nil
+}
+
+// main2 calls main3 and handles any errors it returns. It takes its output
+// streams and command-line arguments and even main3 as parameters to support
+// testability.
+func main2(
+	stdout, stderr io.Writer, args []string,
+	main3 func(stdout, stderr io.Writer, args []string) error,
+) int {
+	if err := main3(stdout, stderr, args); err != nil {
+		fmt.Fprintln(stderr, err.Error())
+		if err, ok := err.(exit); ok {
+			return err.code
+		}
+		return 1
+	}
+	return 0
+}
+
+// main is as small as possible to minimise its no-coverage footprint.
+func main() {
+	if rc := main2(os.Stdout, os.Stderr, os.Args, main3); rc != 0 {
+		os.Exit(rc)
 	}
 }
--- sysl2/sysl/sysl_test.go
+++ sysl2/sysl/sysl_test.go
@@ -1,10 +1,62 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/anz-bank/sysl/src/proto"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+type testTempFile struct {
+	f *os.File
+}
+
+func newTestTempFile(t *testing.T, dir, pattern string) *testTempFile {
+	f, err := ioutil.TempFile("", "github.com-sysl-sysl2-sysl-sysl_test.go-TestJSONPB-*.json")
+	if assert.NoError(t, err, "newTestTempFile(%#v, %#v)", dir, pattern) {
+		return &testTempFile{f}
+	}
+	return nil
+}
+
+func testTempFilename(t *testing.T, dir, pattern string) string {
+	if tf := newTestTempFile(t, dir, pattern); tf != nil {
+		defer tf.CloseAndRemove()
+		return tf.Name()
+	}
+	return ""
+}
+
+func (tf *testTempFile) File() *os.File {
+	return tf.f
+}
+
+func (tf *testTempFile) Name() string {
+	return tf.f.Name()
+}
+
+func (tf *testTempFile) CloseAndRemove() {
+	tf.f.Close()
+	os.Remove(tf.Name())
+}
+
+func TestExit(t *testing.T) {
+	format := "Exiting: %s"
+	param := "Oopsies!"
+	message := fmt.Sprintf(format, param)
+	code := 42
+	e := exitf(code, format, param)
+	assert.Error(t, e)
+	assert.Equal(t, message, e.message)
+	assert.Equal(t, message, e.Error())
+	assert.Equal(t, 42, e.code)
+}
 
 func TestOSFileSystem(t *testing.T) {
 	fs := &osFileSystem{root: "."}
@@ -26,4 +78,211 @@ func TestFSFileStream(t *testing.T) {
 	s, err := newFSFileStream("sysl.go", fs)
 	assert.NoError(t, err)
 	assert.Equal(t, "package main", s.GetText(0, 11))
+}
+
+var testModule = &sysl.Module{
+	Apps: map[string]*sysl.Application{
+		"Test": &sysl.Application{
+			Name: &sysl.AppName{
+				Part: []string{"Test"},
+			},
+			Endpoints: map[string]*sysl.Endpoint{
+				"GetInfo": &sysl.Endpoint{
+					Name: "GetInfo",
+					Stmt: []*sysl.Statement{
+						{
+							Stmt: &sysl.Statement_Action{
+								Action: &sysl.Action{
+									Action: "Do something",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+var testModuleJSONPB = `{
+ "apps": {
+  "Test": {
+   "name": {
+    "part": [
+     "Test"
+    ]
+   },
+   "endpoints": {
+    "GetInfo": {
+     "name": "GetInfo",
+     "stmt": [
+      {
+       "action": {
+        "action": "Do something"
+       }
+      }
+     ]
+    }
+   }
+  }
+ }
+}`
+
+var testModuleTextPB = `apps: <
+  key: "Test"
+  value: <
+    name: <
+      part: "Test"
+    >
+    endpoints: <
+      key: "GetInfo"
+      value: <
+        name: "GetInfo"
+        stmt: <
+          action: <
+            action: "Do something"
+          >
+        >
+      >
+    >
+  >
+>
+`
+
+func TestJSONPB(t *testing.T) {
+	if filename := testTempFilename(t, "", "github.com-sysl-sysl2-sysl-sysl_test.go-TestJSONPB-*.json"); filename != "" {
+		JSONPB(testModule, filename)
+		output, err := ioutil.ReadFile(filename)
+		require.NoError(t, err)
+		assert.Equal(t, testModuleJSONPB, string(output))
+	}
+}
+
+func TestJSONPBNilModule(t *testing.T) {
+	if tf := newTestTempFile(t, "", "github.com-sysl-sysl2-sysl-sysl_test.go-TestJSONPB-*.json"); tf != nil {
+		filename := tf.Name()
+		tf.CloseAndRemove()
+		err := JSONPB(nil, filename)
+		_, err = os.Stat(filename)
+		assert.True(t, os.IsNotExist(err))
+	}
+}
+
+func TestFJSONPB(t *testing.T) {
+	var output bytes.Buffer
+	FJSONPB(&output, testModule)
+	assert.Equal(t, testModuleJSONPB, output.String())
+}
+
+func TestFJSONPBNilModule(t *testing.T) {
+	var output bytes.Buffer
+	err := FJSONPB(&output, nil)
+	assert.Error(t, err)
+	assert.Equal(t, "", output.String())
+}
+
+func TestTextPB(t *testing.T) {
+	if filename := testTempFilename(t, "", "github.com-sysl-sysl2-sysl-sysl_test.go-TestJSONPB-*.json"); filename != "" {
+		TextPB(testModule, filename)
+		output, err := ioutil.ReadFile(filename)
+		require.NoError(t, err)
+		assert.Equal(t, testModuleTextPB, string(output))
+	}
+}
+
+func TestTextPBNilModule(t *testing.T) {
+	if tf := newTestTempFile(t, "", "github.com-sysl-sysl2-sysl-sysl_test.go-TestTextPBNilModule-*.textpb"); tf != nil {
+		filename := tf.Name()
+		tf.CloseAndRemove()
+		err := TextPB(nil, filename)
+		assert.Error(t, err)
+		_, err = os.Stat(filename)
+		assert.True(t, os.IsNotExist(err))
+	}
+}
+
+func TestFTextPB(t *testing.T) {
+	var output bytes.Buffer
+	FTextPB(&output, testModule)
+	assert.Equal(t, testModuleTextPB, output.String())
+}
+
+func TestFTextPBNilModule(t *testing.T) {
+	var output bytes.Buffer
+	err := FTextPB(&output, nil)
+	assert.Error(t, err)
+	assert.Equal(t, "", output.String())
+}
+
+func testMain2(t *testing.T, args []string, golden string) {
+	if output := testTempFilename(t, "", "github.com-sysl-sysl2-sysl-sysl_test.go-TestTextPBNilModule-*.textpb"); output != "" {
+		var stdout, stderr bytes.Buffer
+		rc := main2(&stdout, &stderr, append([]string{"sysl", "-o", output}, args...), main3)
+		if !assert.Zero(t, rc) {
+			t.Error(stderr.String())
+		}
+		assert.True(t, stdout.Len() == 0)
+
+		actual, err := ioutil.ReadFile(output)
+		require.NoError(t, err)
+
+		expected, err := ioutil.ReadFile(golden)
+		require.NoError(t, err)
+
+		assert.Equal(t, string(expected), string(actual))
+	}
+}
+
+func TestMain2TextPB(t *testing.T) {
+	testMain2(t, []string{"tests/args.sysl"}, "tests/args.sysl.golden.textpb")
+}
+
+func TestMain2JSON(t *testing.T) {
+	testMain2(t, []string{"-mode", "json", "tests/args.sysl"}, "tests/args.sysl.golden.json")
+}
+
+func testMain2Stdout(t *testing.T, args []string, golden string) {
+	var stdout, stderr bytes.Buffer
+	rc := main2(&stdout, &stderr, append([]string{"sysl", "-o", "-"}, args...), main3)
+	if !assert.Zero(t, rc) {
+		t.Error(stderr.String())
+	}
+
+	expected, err := ioutil.ReadFile(golden)
+	require.NoError(t, err)
+
+	assert.Equal(t, string(expected), stdout.String())
+
+	_, err = os.Stat("-")
+	assert.True(t, os.IsNotExist(err), "Should not have created file '-'")
+}
+
+func TestMain2TextPBStdout(t *testing.T) {
+	testMain2Stdout(t, []string{"tests/args.sysl"}, "tests/args.sysl.golden.textpb")
+}
+
+func TestMain2JSONStdout(t *testing.T) {
+	testMain2Stdout(t, []string{"-mode", "json", "tests/args.sysl"}, "tests/args.sysl.golden.json")
+}
+
+func TestMain2BadMode(t *testing.T) {
+	var stdout, stderr bytes.Buffer
+	rc := main2(&stdout, &stderr, []string{"sysl", "-o", "-", "-mode", "BAD", "tests/args.sysl"}, main3)
+	assert.NotZero(t, rc)
+
+	_, err := os.Stat("-")
+	assert.True(t, os.IsNotExist(err), "Should not have created file '-'")
+}
+
+func TestMain2Fatal(t *testing.T) {
+	var stdout, stderr bytes.Buffer
+	assert.Equal(t, 1, main2(&stdout, &stderr, nil, func(_, _ io.Writer, _ []string) error {
+		return fmt.Errorf("Generic error")
+	}))
+	assert.Equal(t, "Generic error\n", stderr.String())
+	stderr.Reset()
+	assert.Equal(t, 42, main2(&stdout, &stderr, nil, func(_, _ io.Writer, _ []string) error {
+		return exitf(42, "Exit error")
+	}))
+	assert.Equal(t, "Exit error\n", stderr.String())
 }
