91e150dd898dd5c241501670e2b8288623c1a917
JackLi <19960803asd>
2019-03-05 16:43:11 +0800

    Output file from sequence diagram * Encode sequence diagram * Send http request to plantuml * Generate a png/svg/uml file

--- sysl2/sysl/diagutil.go
+++ sysl2/sysl/diagutil.go
@@ -4,64 +4,43 @@ import (
 	"bytes"
 	"compress/zlib"
 	"fmt"
-	"io/ioutil"
+	"image/jpeg"
+	"image/png"
 	"net/http"
 	"os"
 
 	"github.com/sirupsen/logrus"
 )
 
-func OutputPlantuml(output, plantuml, umlInput string) {
-	mode := output[len(output)-3:]
-
-	if plantuml == "" {
-		plantuml = os.Getenv("SYSL_PLANTUML")
+func OutputPlantuml(mode, url string, umlInput []byte) {
+	if url == "" {
+		url = os.Getenv("SYSL_PLANTUML")
 	}
-
-	switch mode {
-	case "png", "svg":
-		plantuml = fmt.Sprintf("%s/%s/%s", plantuml, mode, DeflateAndEncode([]byte(umlInput)))
-		resp, err := http.Get(plantuml)
-		if err != nil {
-			logrus.Errorf("Unable to create http request to %s, Error:%s", plantuml, err.Error())
-		}
-		defer resp.Body.Close()
-
-		out, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			logrus.Errorf("Unable to read from response, Error:%s", err.Error())
-		}
-		createFile(output, out)
-
-	case "uml":
-		createFile(output, []byte(umlInput))
-
-	default:
-		logrus.Errorf("Extension %s not supported. Valid extensions: svg, png, uml.", mode)
-	}
-}
-
-func createFile(output string, out []byte) {
-	f, err := os.Create(output)
+	url = fmt.Sprintf("%s/%s/%s", url, mode, DeflateAndEncode(umlInput))
+	resp, err := http.Get(url)
 	if err != nil {
-		logrus.Errorf("Unable to create file, Error:%s", err.Error())
+		logrus.Errorf("Unable to create http request to %s, Error:", url, err.Error())
 	}
-	_, err = f.Write(out)
+	defer resp.Body.Close()
+
+	img, err := png.Decode(resp.Body)
+	if err != nil {
+		logrus.Errorf("Unable to create image from response, Error:", err.Error())
+	}
+
+	f, err := os.Create("_.jpg")
+	if err != nil {
+		logrus.Errorf("Unable to create file, Error:", err.Error())
+	}
 	defer f.Close()
-
-	if err != nil {
-		logrus.Errorf("Unable to create file, Error:%s", err.Error())
-	}
+	jpeg.Encode(f, img, nil)
 }
 
-//
-// The functions below ported from https://github.com/dougn/python-plantuml/blob/master/plantuml.py
-//
 func DeflateAndEncode(text []byte) string {
 	var buf bytes.Buffer
 	zw, err := zlib.NewWriterLevel(&buf, zlib.BestCompression)
 	if err != nil {
-		logrus.Errorf("Unable to encode []byte, Error:%s", err.Error())
+		logrus.Errorf("Unable to encode []byte, Error:", err.Error())
 	}
 	zw.Write(text)
 	zw.Flush()
@@ -83,7 +62,6 @@ func encode(data []byte) string {
 	return buf.String()
 }
 
-// 3 bytes takes 24 bits. This splits 24 bits into 4 bytes of which lower 6-bit takes account.
 func encode3bytes(buf *bytes.Buffer, b1, b2, b3 byte) {
 	c1 := b1 >> 2
 	c2 := ((b1 & 0x3) << 4) | (b2 >> 4)
@@ -95,28 +73,19 @@ func encode3bytes(buf *bytes.Buffer, b1, b2, b3 byte) {
 	buf.WriteByte(encode6bit(c3 & 0x3F))
 	buf.WriteByte(encode6bit(c4 & 0x3F))
 }
-
 func encode6bit(b byte) byte {
-	// 6 bit makes value 0 to 63. The func maps 0-63 to characters
-	// '0'-'9', 'A'-'Z', 'a'-'z', '-', '_'. '?' should never be reached.
 	if b < 10 {
-		// 48 -> '0'
 		return byte(48 + b)
 	}
 	b -= 10
-
 	if b < 26 {
-		// 65 -> 'A'
 		return byte(65 + b)
 	}
 	b -= 26
-
 	if b < 26 {
-		// 97 -> 'a'
 		return byte(97 + b)
 	}
 	b -= 26
-
 	if b == 0 {
 		return '-'
 	}
@@ -124,5 +93,4 @@ func encode6bit(b byte) byte {
 		return '_'
 	}
 	return '?'
-
 }
