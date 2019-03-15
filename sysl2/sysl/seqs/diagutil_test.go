b8c3741c6762745d8456abc3ef4d46142c31a696
lih16 <hongsheng.li@anz.com>
2019-03-15 17:41:54 +0800

    Sequence diagram generate
    * Fix test case

--- sysl2/sysl/seqs/diagutil_test.go
+++ sysl2/sysl/seqs/diagutil_test.go
@@ -34,7 +34,7 @@ deactivate _0
 
 func TestDeflateAndEncode(t *testing.T) {
 	//Given
-	const expected = "UDfSaKbhmp0GXU_pAnwvYqY6NaniKkXoAgGRFUGW9l4qY7gh99SkzByN9GvnUfBGzmrwZw5bYEpZqDIqxThekngp5zdS-AwDqbOpS83L9tRPkyEReOeZRpW8PbVZxK0o2c-kxTbpWuO_xoG4ticZ-nPa5vgYYxLWvRjNLmiL1IRVOQ7m8E-3X3WAA0fQgz9gvFy8yJQw3uwIyi5gLLg37BVNJvWFGNoO_wJ3kkftteyZECqO0gnHfSsGutuG__KSn1CcIhPN5ahjdH5NSYPOdRWP-J7QcMLedPpKu5XgnJkXgQDfAMsLjl07003__m400F__Rhg13W00"
+	const expected = "UDfSaKbhmp0GXU_pAnwvYqY6NaniKkXoAgGRFUGW9l4qY7gh99SkzByN9GvnUfBGzmrwZw5bYEpZqDIqxThekngp5zdS-AwDqbOpS83L9tRPkyEReOeZRpW8PbVZxK0o2c-kxTbpWuO_xoG4ticZ-nPa5vgYYxLWvRjNLmiL1IRVOQ7m8E-3X3WAA0fQgz9gvFy8yJQw3uwIyi5gLLg37BVNJvWFGNoO_wJ3kkftteyZECqO0gnHfSsGutuG__KSn1CcIhPN5ahjdH5NSYPOdRWP-J7QcMLedPpKu5XgnJkXgQDfAMsLjl0N003__swwWGu0"
 
 	//When
 	actual := DeflateAndEncode([]byte(testPlantumlInput))
@@ -82,19 +82,5 @@ func TestOutputPlantumlWithUml(t *testing.T) {
 
 	//Then
 	_, err := os.Stat("test.puml")
-	assert.False(t, os.IsNotExist(err))
-}
-
-func TestOutputPlantumlWithoutUrl(t *testing.T) {
-	//Given
-	output := "test.uml"
-	plantuml := ""
-	umlInput := testPlantumlInput
-
-	//When
-	OutputPlantuml(output, plantuml, umlInput)
-
-	//Then
-	_, err := os.Stat(output)
 	assert.False(t, os.IsNotExist(err))
 }
