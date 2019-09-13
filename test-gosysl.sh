4938b08516d55ca1b8a8f91706e84f3b55ca4708
Jonathan Gordon <Jonathan.Gordon@anz.com>
2019-09-13 15:42:07 +1000

    Revert

--- test-gosysl.sh
+++ test-gosysl.sh
@@ -4,19 +4,19 @@ set -e
 ROOT="sysl2/sysl/tests"
 for f in $ROOT/*.sysl; do
  f=`basename $f`
- $GOPATH/bin/sysl -v pb --mode textpb --root $ROOT -o $ROOT/$f.out.txt /$f
+ $GOPATH/bin/sysl pb --mode textpb --root $ROOT -o $ROOT/$f.out.txt /$f -v
 done;
 
 rm $ROOT/*.out.txt
 
-$GOPATH/bin/sysl -v sd -a 'Project' $ROOT/sequence_diagram_project.sysl
+$GOPATH/bin/sysl sd -a 'Project' $ROOT/sequence_diagram_project.sysl -v
 rm _.png
 
-$GOPATH/bin/sysl -v sd -s 'WebFrontend <- RequestProfile' -o sd.png $ROOT/sequence_diagram_project.sysl
+$GOPATH/bin/sysl sd -s 'WebFrontend <- RequestProfile' -o sd.png $ROOT/sequence_diagram_project.sysl -v
 rm sd.png
 
-$GOPATH/bin/sysl -v ints -j 'Project' $ROOT/integration_test.sysl
-rm _.png..
+$GOPATH/bin/sysl ints -j 'Project' $ROOT/integration_test.sysl -v
+rm _.png
 
 version=`$GOPATH/bin/sysl --version 2>&1 >/dev/null`
 if [[ ${version} = "unspecified" ]]; then
