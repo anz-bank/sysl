75e4a0efdd1b8aba9353307c95ba0c79dbde5506
Novan <novan.allanadi1211@gmail.com>
2019-10-09 11:59:35 +1100

    tests for rootHandler

--- sysl2/sysl/cmd_runner_test.go
+++ sysl2/sysl/cmd_runner_test.go
@@ -42,60 +42,65 @@ func TestRootHandler(t *testing.T) {
 	logger := logrus.StandardLogger()
 	sysl := kingpin.New("sysl", "System Modelling Language Toolkit")
 	runner := cmdRunner{}
-	err := runner.Configure(sysl)
-	assert.NoError(t, err)
+	runner.Configure(sysl)
+
+	getAbsolute := func(path string) string {
+		abs, _ := filepath.Abs(path)
+		return abs
+	}
 
 	tests := []map[string]interface{}{
 		{
-			"root":           "",
-			"module":         "tests/root_finder_tests/SuccessfulTest/path/to/module/test.sysl",
-			"relativeOutput": "tests/root_finder_tests/SuccessfulTest",
-			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+			"root":   "",
+			"module": "tests/root_finder_tests/NoSyslModuleTest/fake.sysl",
+			"err":    func(err error) { assert.EqualError(t, err, "Sysl module not found") },
+			"output": "",
 		},
 		{
-			"root":           "",
-			"module":         "tests/root_finder_tests/SuccessfulTest/test2.sysl",
-			"relativeOutput": "tests/root_finder_tests/SuccessfulTest",
-			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+			"root":   "",
+			"module": "tests/root_finder_tests/RootFlagAndSyslRootUndefinedTest/test.sysl",
+			"err":    func(err error) { assert.EqualError(t, err, "Project root is undefined") },
+			"output": "",
 		},
 		{
-			"root":           "",
-			"module":         "tests/root_finder_tests/SuccessfulTest/path/to/another/module/test3.sysl",
-			"relativeOutput": "tests/root_finder_tests/SuccessfulTest/path/to/another",
-			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/path/to/module/test.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest"),
 		},
 		{
-			"root":           "./tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/",
-			"module":         "path/to/module/test.sysl",
-			"relativeOutput": "./tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/",
-			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/test2.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest"),
 		},
 		{
-			"root":           ".",
-			"module":         "path/to/module/test.sysl",
-			"relativeOutput": ".",
-			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/path/to/another/module/test3.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest/path/to/another"),
 		},
 		{
-			"root":           "",
-			"module":         "tests/root_finder_tests/UndefinedRootAndUndefinedSyslRoot/test.sysl",
-			"relativeOutput": "",
-			"error":          func(t *testing.T, err error) { assert.EqualError(t, err, "root is not defined") },
+			"root":   getAbsolute("tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/"),
+			"module": "tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/path/to/module/test.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/"),
 		},
 	}
 
 	for i, test := range tests {
+		t.Logf("Test %d\n", i)
 		runner.Root = test["root"].(string)
 		runner.module = test["module"].(string)
-		realAbsOutput, err := filepath.Abs(test["root"].(string))
-		assert.NoError(t, err)
+		absolutePath, _ := filepath.Abs(runner.module)
+		t.Logf("%s\n", absolutePath)
+		err := runner.rootHandler(fs, logger)
 
-		err = runner.rootHandler(fs, logger)
+		t.Log(err)
+		assert.Equal(t, test["output"], runner.Root)
 
-		t.Logf("Test #%d, root %s", i, realAbsOutput)
-		assert.Equal(t, test["relativeOutput"], runner.Root)
+		// Calling the appropriate error assertion
+		test["err"].(func(err error))(err)
 
-		// testing the appropriate error assertion
-		test["error"].(func(t *testing.T, err error))(t, err)
 	}
 }
