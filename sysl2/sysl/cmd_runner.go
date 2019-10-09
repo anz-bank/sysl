6522a852030649eeb5ef9e0c6ea55db33f635e8f
Novan <novan.allanadi1211@gmail.com>
2019-10-09 12:00:08 +1100

    completed and integrated root handler

--- sysl2/sysl/cmd_runner.go
+++ sysl2/sysl/cmd_runner.go
@@ -20,7 +20,6 @@ import (
 
 const (
 	syslRootMarker = ".SYSL_ROOT"
-	rootUndefined  = "\000"
 )
 
 type cmdRunner struct {
@@ -61,7 +60,7 @@ func (r *cmdRunner) rootHandler(fs afero.Fs, logger *logrus.Logger) error {
 }
 
 func (r *cmdRunner) Configure(app *kingpin.Application) error {
-	// app.UsageTemplate(kingpin.SeparateOptionalFlagsUsageTemplate)
+	app.UsageTemplate(kingpin.SeparateOptionalFlagsUsageTemplate)
 
 	commands := []Command{
 		&protobuf{},
@@ -85,7 +84,6 @@ func (r *cmdRunner) Configure(app *kingpin.Application) error {
 	})
 	for _, cmd := range commands {
 		c := cmd.Configure(app)
-		// TODO: find root based on the sysl module
 		if cmd.RequireSyslModule() {
 			c.Arg("MODULE", "input files without .sysl extension and with leading /, eg: "+
 				"/project_dir/my_models combine with --root if needed").
