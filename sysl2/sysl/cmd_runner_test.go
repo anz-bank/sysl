68b5313a3f672a541b4d13883730b3d043360ced
Novan <novan.allanadi1211@gmail.com>
2019-10-09 11:59:35 +1100

    tests for rootHandler

--- sysl2/sysl/cmd_runner_test.go
+++ sysl2/sysl/cmd_runner_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	// "errors"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -32,4 +36,72 @@ func TestEnsureFlagsNonEmpty(t *testing.T) {
 	args := []string{"foo", "--bar", ""}
 	_, err := sysl.ParseContext(args)
 	assert.Error(t, err)
+}
+
+func TestRootHandler(t *testing.T) {
+	fs := afero.NewOsFs()
+	logger := logrus.StandardLogger()
+	sysl := kingpin.New("sysl", "System Modelling Language Toolkit")
+	runner := cmdRunner{}
+	runner.Configure(sysl)
+
+	getAbsolute := func(path string) string {
+		abs, _ := filepath.Abs(path)
+		return abs
+	}
+
+	tests := []map[string]interface{}{
+		{
+			"root":   "",
+			"module": "tests/root_finder_tests/NoSyslModuleTest/fake.sysl",
+			"err":    func(err error) { assert.EqualError(t, err, "Sysl module not found") },
+			"output": "",
+		},
+		{
+			"root":   "",
+			"module": "tests/root_finder_tests/RootFlagAndSyslRootUndefinedTest/test.sysl",
+			"err":    func(err error) { assert.EqualError(t, err, "Project root is undefined") },
+			"output": "",
+		},
+		{
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/path/to/module/test.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest"),
+		},
+		{
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/test2.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest"),
+		},
+		{
+			"root":   "",
+			"module": "tests/root_finder_tests/SuccessfulTest/path/to/another/module/test3.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/SuccessfulTest/path/to/another"),
+		},
+		{
+			"root":   getAbsolute("tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/"),
+			"module": "tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/path/to/module/test.sysl",
+			"err":    func(err error) { assert.NoError(t, err) },
+			"output": getAbsolute("tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/"),
+		},
+	}
+
+	for i, test := range tests {
+		t.Logf("Test %d\n", i)
+		runner.Root = test["root"].(string)
+		runner.module = test["module"].(string)
+		absolutePath, _ := filepath.Abs(runner.module)
+		t.Logf("%s\n", absolutePath)
+		err := runner.rootHandler(fs, logger)
+
+		t.Log(err)
+		assert.Equal(t, test["output"], runner.Root)
+
+		// Calling the appropriate error assertion
+		test["err"].(func(err error))(err)
+
+	}
 }
