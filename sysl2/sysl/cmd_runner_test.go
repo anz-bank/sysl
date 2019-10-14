8c31a9e3b0be75b538cefddd64042e29a622ffc8
Novan <novan.allanadi1211@gmail.com>
2019-10-14 13:40:48 +1100

    fixed golang ci

--- sysl2/sysl/cmd_runner_test.go
+++ sysl2/sysl/cmd_runner_test.go
@@ -42,57 +42,58 @@ func TestRootHandler(t *testing.T) {
 	logger := logrus.StandardLogger()
 	sysl := kingpin.New("sysl", "System Modelling Language Toolkit")
 	runner := cmdRunner{}
-	runner.Configure(sysl)
+    err := runner.Configure(sysl)
+    assert.NoError(t, err)
 
 	tests := []map[string]interface{}{
 		{
 			"root":           "",
 			"module":         "tests/root_finder_tests/SuccessfulTest/path/to/module/test.sysl",
-            "relativeOutput": "tests/root_finder_tests/SuccessfulTest",
-            "error": func(t *testing.T, err error) {assert.NoError(t, err)},
+			"relativeOutput": "tests/root_finder_tests/SuccessfulTest",
+			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
 		},
 		{
 			"root":           "",
 			"module":         "tests/root_finder_tests/SuccessfulTest/test2.sysl",
-            "relativeOutput": "tests/root_finder_tests/SuccessfulTest",
-            "error": func(t *testing.T, err error) {assert.NoError(t, err)},
+			"relativeOutput": "tests/root_finder_tests/SuccessfulTest",
+			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
 		},
 		{
 			"root":           "",
 			"module":         "tests/root_finder_tests/SuccessfulTest/path/to/another/module/test3.sysl",
-            "relativeOutput": "tests/root_finder_tests/SuccessfulTest/path/to/another",
-            "error": func(t *testing.T, err error) {assert.NoError(t, err)},
+			"relativeOutput": "tests/root_finder_tests/SuccessfulTest/path/to/another",
+			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
 		},
 		{
 			"root":           "./tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/",
 			"module":         "path/to/module/test.sysl",
-            "relativeOutput": "./tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/",
-            "error": func(t *testing.T, err error) {assert.NoError(t, err)},
-        },
-        {
+			"relativeOutput": "./tests/root_finder_tests/DefinedRootAndSyslRootUndefinedTest/",
+			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+		},
+		{
 			"root":           ".",
 			"module":         "path/to/module/test.sysl",
-            "relativeOutput": ".",
-            "error": func(t *testing.T, err error) {assert.NoError(t, err)},
-        },
-        {
-            "root": "",
-            "module": "tests/root_finder_tests/UndefinedRootAndUndefinedSyslRoot/test.sysl",
-            "relativeOutput": "",
-            "error": func(t *testing.T, err error) {assert.EqualError(t, err, "Root is not defined")},
-        },
+			"relativeOutput": ".",
+			"error":          func(t *testing.T, err error) { assert.NoError(t, err) },
+		},
+		{
+			"root":           "",
+			"module":         "tests/root_finder_tests/UndefinedRootAndUndefinedSyslRoot/test.sysl",
+			"relativeOutput": "",
+			"error":          func(t *testing.T, err error) { assert.EqualError(t, err, "root is not defined") },
+		},
 	}
 
 	for i, test := range tests {
 		runner.Root = test["root"].(string)
 		runner.module = test["module"].(string)
 		err := runner.rootHandler(fs, logger)
-        realAbsOutput, _ := filepath.Abs(test["root"].(string))
+		realAbsOutput, _ := filepath.Abs(test["root"].(string))
 
 		t.Logf("Test #%d, root %s", i, realAbsOutput)
-        assert.Equal(t, test["relativeOutput"], runner.Root)
+		assert.Equal(t, test["relativeOutput"], runner.Root)
 
-        // testing the appropriate error assertion
+		// testing the appropriate error assertion
 		test["error"].(func(t *testing.T, err error))(t, err)
 	}
 }
