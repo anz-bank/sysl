7685a54dee41bc914458c3876c369cc17f6dc6a5
Novan <novan.allanadi1211@gmail.com>
2019-10-29 16:16:37 +1100

    added import checker

---
+++ sysl2/sysl/syslutil/chroot_test.go
@@ -0,0 +1,90 @@
+package syslutil
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/require"
+)
+
+type testStructure struct {
+	name, root, module string
+	folders, files     []string
+	expectedErr        error
+}
+
+func buildFolderTest(folders, files []string) (fs afero.Fs, err error) {
+	fs = afero.NewMemMapFs()
+	var folder, file string
+
+	for _, folder = range folders {
+		folder, err = filepath.Abs(folder)
+		if err != nil {
+			return
+		}
+
+		err = fs.MkdirAll(folder, os.ModeTemporary)
+		if err != nil {
+			return
+		}
+	}
+
+	for _, file = range files {
+		file, err = filepath.Abs(file)
+		if err != nil {
+			return
+		}
+
+		_, err = fs.Create(file)
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+func TestOpen(t *testing.T) {
+	tests := []testStructure{
+		{
+			name:   "import above root",
+			root:   "/a",
+			module: "../bad.sysl",
+			folders: []string{
+				"/a",
+			},
+			files: []string{
+				"/bad.sysl",
+			},
+			expectedErr: errors.New("import can not go up the directory"),
+		},
+		{
+			name:   "successful import",
+			root:   "/a",
+			module: "/good.sysl",
+			folders: []string{
+				"/a",
+			},
+			files: []string{
+				"/a/good.sysl",
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		fs, err := buildFolderTest(test.folders, test.files)
+		t.Run(test.name, func(ts testStructure) func(t *testing.T) {
+			return func(tt *testing.T) {
+				tt.Parallel()
+				require.NoError(tt, err)
+				chrootfs := NewChrootFs(fs, ts.root)
+				_, err = chrootfs.Open(ts.module)
+				require.Equal(tt, ts.expectedErr, err)
+			}
+		}(test))
+	}
+}
