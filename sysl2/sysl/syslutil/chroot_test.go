41e98c12b01ba9df4e60d7fb1953c2472ca02b07
Novan <novan.allanadi1211@gmail.com>
2019-11-06 10:32:56 +1100

    added relative import test, fixed deeply nested structure, and edge cases test

--- sysl2/sysl/syslutil/chroot_test.go
+++ sysl2/sysl/syslutil/chroot_test.go
@@ -74,27 +74,89 @@ func TestOpen(t *testing.T) {
 			expectedErr: nil,
 		},
 		{
-			name:   "deeply nested structure",
+			name:   "deeply nested structure with system root as root",
 			root:   "/",
 			module: "../../bar.sysl",
 			folders: []string{
 				"/a/b/c/d/e",
 			},
 			files: []string{
-				"bar.sysl",
+				"/bar.sysl",
+				"/a/b/c/d/e/foo.sysl",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "deeply nested structure with directory as a root",
+			root:   "/a/b",
+			module: "../../bar.sysl",
+			folders: []string{
+				"/a/b/c/d/e",
+			},
+			files: []string{
+				"/bar.sysl",
 				"/a/b/c/d/e/foo.sysl",
 			},
 			expectedErr: errors.New("permission denied, file outside root"),
 		},
+		{
+			name:   "relative import",
+			root:   ".",
+			module: "/good.sysl",
+			folders: []string{
+				".",
+			},
+			files: []string{
+				"./good.sysl",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "failed relative import",
+			root:   "./a/b",
+			module: "../bad.sysl",
+			folders: []string{
+				"./a/b",
+			},
+			files: []string{
+				"./a/bad.sysl",
+			},
+			expectedErr: errors.New("permission denied, file outside root"),
+		},
+		{
+			name:   "strange file name",
+			root:   "../a",
+			module: "/..strange.sysl",
+			folders: []string{
+				"../a/",
+			},
+			files: []string{
+				"../a/..strange.sysl",
+			},
+			expectedErr: nil,
+		},
+		{
+			name:   "strange path",
+			root:   "../a",
+			module: "/b/../..strange.sysl",
+			folders: []string{
+				"../a/b",
+			},
+			files: []string{
+				"../a/..strange.sysl",
+			},
+			expectedErr: nil,
+		},
 	}
 
 	for _, test := range tests {
-		fs, err := buildFolderTest(test.folders, test.files)
 		t.Run(test.name, func(ts testStructure) func(t *testing.T) {
 			return func(tt *testing.T) {
+				fs, err := buildFolderTest(ts.folders, ts.files)
 				tt.Parallel()
 				require.NoError(tt, err)
-				chrootfs := NewChrootFs(fs, ts.root)
+				chrootfs, err := NewChrootFs(fs, ts.root)
+				require.NoError(tt, err)
 				_, err = chrootfs.Open(ts.module)
 				require.Equal(tt, ts.expectedErr, err)
 			}
