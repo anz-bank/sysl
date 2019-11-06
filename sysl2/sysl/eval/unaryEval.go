6caff61bce654304fe5acacd0070c40591b85b4c
Jonathan Gordon <Jonathan.Gordon@anz.com>
2019-11-06 20:42:35 +1100

    Add an unary string operator to the internal language to convert a value to string

--- sysl2/sysl/eval/unaryEval.go
+++ sysl2/sysl/eval/unaryEval.go
@@ -1,6 +1,9 @@
 package eval
 
 import (
+	"fmt"
+	"strings"
+
 	sysl "github.com/anz-bank/sysl/src/proto"
 	"github.com/pkg/errors"
 )
@@ -11,6 +14,7 @@ type unaryFunc func(*sysl.Value) *sysl.Value
 var unaryFunctions = map[sysl.Expr_UnExpr_Op]unaryFunc{
 	sysl.Expr_UnExpr_NEG:    unaryNeg,
 	sysl.Expr_UnExpr_SINGLE: unarySingle,
+	sysl.Expr_UnExpr_STRING: unaryString,
 }
 
 func evalUnaryFunc(op sysl.Expr_UnExpr_Op, arg *sysl.Value) *sysl.Value {
@@ -49,4 +53,27 @@ func unarySingle(list *sysl.Value) *sysl.Value {
 		panic(errors.Errorf("unarySingle expecting array length 1, got %v", len(v)))
 	}
 	return v[0]
+}
+
+func unaryString(arg *sysl.Value) *sysl.Value {
+	listfn := func(items []*sysl.Value) *sysl.Value {
+		var parts []string
+		for _, item := range items {
+			parts = append(parts, unaryString(item).GetS())
+		}
+		return MakeValueString(fmt.Sprintf("[%s]", strings.Join(parts, ", ")))
+	}
+	switch x := arg.Value.(type) {
+	case *sysl.Value_S:
+		return arg
+	case *sysl.Value_I:
+		return MakeValueString(fmt.Sprintf("%d", x.I))
+	case *sysl.Value_B:
+		return MakeValueString(map[bool]string{true: "true", false: "false"}[x.B])
+	case *sysl.Value_List_:
+		return listfn(x.List.GetValue())
+	case *sysl.Value_Set:
+		return listfn(x.Set.GetValue())
+	}
+	return MakeValueString(arg.String())
 }
