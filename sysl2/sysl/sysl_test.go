e2d1c4a74958bfe82148db4a373b6ffe6f4a8bdf
Sumit Narayan <sumit.narayan@anz.com>
2019-11-19 17:33:51 +1100

    Dump all rules in the grammar, Detect grammar type

--- sysl2/sysl/sysl_test.go
+++ sysl2/sysl/sysl_test.go
@@ -884,8 +884,7 @@ func TestCodegenGrammarImportDefOut(t *testing.T) {
 	t.Parallel()
 	logger, _ := test.NewNullLogger()
 	memFs, fs := syslutil.WriteToMemOverlayFs(".")
-	main2([]string{"sysl", "import", "-i", "importer/tests-grammar/go.gen.g", "-s", "goFile", "-a",
-		"Go"}, fs, logger, main3)
+	main2([]string{"sysl", "import", "-i", "importer/tests-grammar/go.gen.g"}, fs, logger, main3)
 	syslutil.AssertFsHasExactly(t, memFs, "/output.sysl")
 }
 
@@ -893,7 +892,6 @@ func TestCodegenGrammarImport(t *testing.T) {
 	t.Parallel()
 	logger, _ := test.NewNullLogger()
 	memFs, fs := syslutil.WriteToMemOverlayFs(".")
-	main2([]string{"sysl", "import", "-i", "importer/tests-grammar/go.gen.g", "-s", "goFile", "-a",
-		"Go", "-o", "out.sysl"}, fs, logger, main3)
+	main2([]string{"sysl", "import", "-i", "importer/tests-grammar/go.gen.g", "-o", "out.sysl"}, fs, logger, main3)
 	syslutil.AssertFsHasExactly(t, memFs, "/out.sysl")
 }
