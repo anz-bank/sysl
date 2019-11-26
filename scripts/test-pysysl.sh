a5f58f0bd7f9e1419ae245ffacb610166eeb8d7f
joshcarp <joshuacarpeggiani@gmail.com>
2019-11-27 10:58:07 +1100

    Remove more python files

--- scripts/test-pysysl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-SYSL_PYTHON_DIR="$1"
-
-flake8
-pytest test/e2e --syslexe=${SYSL_PYTHON_DIR}sysl --reljamexe=${SYSL_PYTHON_DIR}reljam
-coverage run --source=src/sysl -m py.test
