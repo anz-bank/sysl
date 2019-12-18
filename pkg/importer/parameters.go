a88b008b7338ea95406d54bb687d3fdbd1e1fc98
Jonathan Gordon <Jonathan.Gordon@anz.com>
2019-12-18 17:02:59 +1100

    Switch the swagger importer to convert to openAPI3 for input conversion

--- pkg/importer/parameters.go
+++ pkg/importer/parameters.go
@@ -1,13 +1,5 @@
 package importer
 
-import (
-	"fmt"
-
-	"github.com/sirupsen/logrus"
-
-	"github.com/go-openapi/spec"
-)
-
 type Param struct {
 	Field
 	In string
@@ -56,73 +48,4 @@ func (p Parameters) BodyParams() []Param {
 }
 func (p Parameters) PathParams() []Param {
 	return p.findParams("path")
-}
-
-func buildParam(param spec.Parameter, types TypeList, globals Parameters, logger *logrus.Logger) (Param, error) {
-	fromType := func(t Type) Param {
-		return Param{
-			Field: Field{
-				Name:     param.Name,
-				Optional: !param.Required,
-				Type:     t,
-			},
-			In: param.In,
-		}
-	}
-	fromString := func(typeName string) Param {
-		t, found := types.Find(typeName)
-		if !found {
-			logger.Errorf("referenced parameter type %s not found\n", typeName)
-			t = NewStringAlias(typeName)
-		}
-		return fromType(t)
-	}
-
-	paramTypeName := param.Type
-	if paramTypeName == "" {
-		if param.Schema != nil {
-			if ptype, ok := types.findFromSchema(*param.Schema, &typeData{logger: logger}); ok {
-				return fromType(ptype), nil
-			}
-			return Param{}, fmt.Errorf("referenced parameter type not found: %+v", param.Schema)
-		} else if refURL := param.Ref.GetURL(); refURL != nil {
-			refParamName := getReferenceFragment(refURL)
-			if p, ok := globals.items[refParamName]; ok {
-				return p, nil
-			}
-			return Param{}, fmt.Errorf("referenced parameter %s unknown", refParamName)
-		}
-	} else if paramTypeName == StringTypeName {
-		return fromString(paramTypeName), nil
-	}
-	return fromString(mapSwaggerTypeAndFormatToType(paramTypeName, param.Format, logger)), nil
-}
-
-func buildParameters(params []spec.Parameter, types TypeList,
-	globals Parameters, baseParams Parameters, logger *logrus.Logger) Parameters {
-	res := baseParams
-	for _, param := range params {
-		p, err := buildParam(param, types, globals, logger)
-		if err != nil {
-			logger.Errorf("%s", err.Error())
-		} else {
-			res.Add(p)
-		}
-	}
-
-	return res
-}
-
-func buildGlobalParams(params map[string]spec.Parameter, types TypeList, logger *logrus.Logger) Parameters {
-	res := Parameters{}
-	for key, param := range params {
-		p, err := buildParam(param, types, res, logger)
-		if err != nil {
-			logger.Errorf("%s\n", err.Error())
-		} else {
-			p.Name = key
-			res.Add(p)
-		}
-	}
-	return res
 }
