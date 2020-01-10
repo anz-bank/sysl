80b97da8dec2594d0c4187c98f3afc93450475d6
Chloe Zhang <cikeyplanet@gmail.com>
2020-01-10 16:17:33 +1100

    test: mod filesystem

---
+++ pkg/mod/fs_test.go
@@ -0,0 +1,48 @@
+package mod
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewFs(t *testing.T) {
+	t.Parallel()
+
+	backendFs := afero.NewOsFs()
+	fs := NewFs(backendFs)
+	assert.Equal(t, backendFs, fs.fs)
+}
+
+func TestOpenLocalFile(t *testing.T) {
+	t.Parallel()
+
+	filename := "github.com/anz-bank/sysl/demo/examples/Modules/deps.sysl"
+	fs := NewFs(afero.NewOsFs())
+	f, err := fs.Open(filename)
+	assert.Nil(t, err)
+	assert.Equal(t, "deps.sysl", filepath.Base(f.Name()))
+}
+
+func TestOpenRemoteFile(t *testing.T) {
+	t.Parallel()
+
+	filename := "github.com/ChloePlanet/sysltestpub/pubbanana.sysl"
+	fs := NewFs(afero.NewOsFs())
+	f, err := fs.Open(filename)
+	assert.Nil(t, err)
+	assert.Equal(t, "pubbanana.sysl", filepath.Base(f.Name()))
+}
+
+func TestOpenRemoteFileFailed(t *testing.T) {
+	t.Parallel()
+
+	filename := "github.com/wrong/sysltestpub/pubbanana.sysl"
+	fs := NewFs(afero.NewOsFs())
+	f, err := fs.Open(filename)
+	assert.Nil(t, f)
+	assert.Equal(t, fmt.Sprintf("%s not found", filename), err.Error())
+}
