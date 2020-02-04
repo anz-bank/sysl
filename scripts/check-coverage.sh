7036022f537f61040ac98c33f7dbf3ca74c3a5cc
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-02-04 15:43:55 +1100

    Fixes #564
    fixed the broken code

--- scripts/check-coverage.sh
+++ scripts/check-coverage.sh
@@ -17,10 +17,13 @@ coverage_level() {
 }
 
 go test -coverprofile=$COVERAGE_FILE -covermode=atomic ./...
+EXIT_CODE=$(echo $?)
 
 COVERAGE_LEVEL="$(coverage_level)"
 if [ "$COVERAGE_LEVEL" -lt "$MIN_COVERAGE" ]; then
     printf "\e[1;31m✘\e[0m Coverage %d%% < %d%% required\n" ${COVERAGE_LEVEL} ${MIN_COVERAGE}
-    exit 1
+else
+    printf "\e[1;32m✔\e[0m Coverage %d%% >= %d%% required\n" ${COVERAGE_LEVEL} ${MIN_COVERAGE}
 fi
-printf "\e[1;32m✔\e[0m Coverage %d%% >= %d%% required\n" ${COVERAGE_LEVEL} ${MIN_COVERAGE}
+
+exit $EXIT_CODE
