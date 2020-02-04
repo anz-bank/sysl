4d31dec1e8102ffaeac378f1780ec49632d49b31
Chloe Zhang <cikeyplanet@gmail.com>
2020-02-04 17:14:38 +1100

    feat: move binaryinfo variables from package cfg to main

---
+++ cmd/sysl/binaryinfo.go
@@ -0,0 +1,15 @@
+package main
+
+// Version   - Binary version
+// GitCommit - Commit SHA of the source code
+// BuildDate - Binary build date
+// GoVersion - Binary build GoVersion
+// BuildOS   - Operating System used to build binary
+//nolint:gochecknoglobals
+var (
+	Version   = "unspecified"
+	GitCommit = "unspecified"
+	BuildDate = "unspecified"
+	GoVersion = "unspecified"
+	BuildOS   = "unspecified"
+)
