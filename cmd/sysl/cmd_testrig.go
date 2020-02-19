a3f1e30624d72c3c47ae3c53dff305962d2deb47
Alexander Eresov <alexander.eresov@anx.com>
2020-02-20 10:45:41 +1100

    wip

--- cmd/sysl/cmd_testrig.go
+++ cmd/sysl/cmd_testrig.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	test_rig "github.com/anz-bank/sysl/pkg/test_rig"
+	"github.com/anz-bank/sysl/pkg/cmdutils"
+	"github.com/anz-bank/sysl/pkg/test_rig"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 type testRigCmd struct {
-	CmdContextParamTestRig
+	cmdutils.CmdContextParamTestRig
 }
 
 func (p *testRigCmd) Name() string {
@@ -19,19 +20,19 @@ func (p *testRigCmd) MaxSyslModule() int {
 
 func (p *testRigCmd) Configure(app *kingpin.Application) *kingpin.CmdClause {
 	cmd := app.Command(p.Name(), "Generate test rig")
-	cmd.Flag("template", "variables file name (json)").Required().StringVar(&p.templateFileName)
-	cmd.Flag("output-dir", "directory to put generated files").StringVar(&p.outputDir)
+	cmd.Flag("template", "variables file name (json)").Required().StringVar(&p.TemplateFileName)
+	cmd.Flag("output-dir", "directory to put generated files").StringVar(&p.OutputDir)
 	EnsureFlagsNonEmpty(cmd)
 	return cmd
 }
 
-func (p *testRigCmd) Execute(args ExecuteArgs) error {
+func (p *testRigCmd) Execute(args cmdutils.ExecuteArgs) error {
 	var err error
 	err = refineCmd(p)
 	if err != nil {
 		return err
 	}
-	err = test_rig.GenerateRig(p.templateFileName, p.outputDir, args.Modules)
+	err = test_rig.GenerateRig(p.TemplateFileName, p.OutputDir, args.Modules)
 	if err != nil {
 		return err
 	}
@@ -40,7 +41,6 @@ func (p *testRigCmd) Execute(args ExecuteArgs) error {
 
 func refineCmd(p *testRigCmd) error {
 	// vars file should exist
-	// app-name should match what we have in sysl modules
 
 	return nil
 }
