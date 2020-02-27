0e6ec8e00f1c98a28a430a8e9f11a9f4725ee1d6
Zhang Eric <Xing.Zhang@anz.com>
2020-02-27 16:03:31 +0800

    #619 update test data and test case.

--- pkg/syslutil/cmd_config_test.go
+++ pkg/syslutil/cmd_config_test.go
@@ -10,10 +10,11 @@ func TestReadCMDFlags(t *testing.T) {
 	t.Parallel()
 	flags, err := ReadCMDFlags("tests/config.txt")
 	assert.Equal(t, nil, err)
-	assert.Equal(t, 3, len(flags))
+	assert.Equal(t, 4, len(flags))
 	assert.Equal(t, "--grammar=go.gen.g", flags[0])
 	assert.Equal(t, "--transform=go.gen.sysl", flags[1])
 	assert.Equal(t, "model.sysl", flags[2])
+	assert.Equal(t, "--app-name=Test", flags[3])
 }
 
 func TestPopulateCMDFlagsFromFile(t *testing.T) {
@@ -21,10 +22,11 @@ func TestPopulateCMDFlagsFromFile(t *testing.T) {
 	cmdArgs := []string{"sysl", "codegen", "@tests/config.txt"}
 	flags, err := PopulateCMDFlagsFromFile(cmdArgs)
 	assert.Equal(t, nil, err)
-	assert.Equal(t, 5, len(flags))
+	assert.Equal(t, 6, len(flags))
 	assert.Equal(t, "sysl", flags[0])
 	assert.Equal(t, "codegen", flags[1])
 	assert.Equal(t, "--grammar=go.gen.g", flags[2])
 	assert.Equal(t, "--transform=go.gen.sysl", flags[3])
 	assert.Equal(t, "model.sysl", flags[4])
+	assert.Equal(t, "--app-name=Test", flags[5])
 }
