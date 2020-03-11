c5d9af501a6b27228b8ecd21102a8851ad16c69d
aeresov <aeresov@users.noreply.github.com>
2020-03-11 14:33:15 +1100

    first scratchy version of test rig generation (#673)
    Co-authored-by: Alexander Eresov <alexander.eresov@anx.com>

---
+++ pkg/testrig/types.go
@@ -0,0 +1,20 @@
+package testrig
+
+type ServiceVarsMap map[string]ServiceVars
+
+type serviceVarsBase struct {
+	Name   string `json:"name"`
+	Import string `json:"import"`
+}
+
+type ServiceVars struct {
+	serviceVarsBase
+	Port string          `json:"port"`
+	Impl ServiceImplVars `json:"impl"`
+}
+
+type ServiceImplVars struct {
+	serviceVarsBase
+	InterfaceFactory string `json:"interface_factory"`
+	CallbackFactory  string `json:"callback_factory"`
+}
