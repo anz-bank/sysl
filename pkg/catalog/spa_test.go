675788d1a496744fd96a367945c65ecfeba0823a
Jack Zheng <jack.zheng@anz.com>
2020-03-11 22:50:12 +1100

    Add test for spa

--- pkg/catalog/spa_test.go
+++ pkg/catalog/spa_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSpaHandler(t *testing.T) {
@@ -16,4 +18,14 @@ func TestSpaHandler(t *testing.T) {
 	if w.Result().StatusCode != 200 {
 		t.Errorf("Not returning 200")
 	}
+}
+
+func TestSpaHandlerNonExistent(t *testing.T) {
+	// Test we can handle rest spec
+	req := httptest.NewRequest(http.MethodGet, "/doesnotexist.json", nil)
+	w := httptest.NewRecorder()
+	handler := spaHandler{staticPath: "/ui/build", indexPath: "/ui/build/index.html"}
+	handler.ServeHTTP(w, req)
+	//nolint:bodyclose
+	assert.Equal(t, 500, w.Result().StatusCode, "expected status code to be equal")
 }
