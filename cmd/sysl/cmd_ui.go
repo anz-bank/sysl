6bc72ff6aeeb1f03d7cff1fb248a59d30da8fa3a
Jack Zheng <j.zheng@live.com.au>
2020-03-13 11:48:52 +1100

    Sysl UI Command (#639)
    * Add react files for sysl-ui
    
    * Add static compiled files from pkger
    
    * Add test files for sysl ui
    
    * Add .DS_Store to gitignore
    
    * Add sysl ui command
    
    * Run Go mod tidy
    
    * Ignore static file pkged.go in code coverage
    
    * Add grocerystore example sysl app
    
    * Add test for catalog.go
    
    * Refactor catalog
    
    * Add server_test.go
    
    * Fix linter issue
    
    * Add single page app handler tests
    
    * Add flag to skip grpc handler generation
    
    * Fix issue where grpc dial wasn't timing out
    
    * Add test for spa
    
    * Rename catalog to ui
    
    * Cleanup unused function
    
    * Fix fields in UI for displaying service information
    
    * Attempt to fix spa_test.go failure on windows
    
    https://github.com/anz-bank/sysl/pull/639/checks?check_run_id=501981830
    
    Appears to be some issue with the backslashes being read by pkger incorrectly. Using filepath.FromSlash replaces the slashes with a separator character, which is platform agnostic, which will hopefully fix the broken test.
    
    * Update grocery store
    
    * Update tests to pass failures on windows
    
    * Remove test that was failing on windows.
    It seems to be an issue with pkger not resolving the files correctly on a windows machine.
    Possible fixes include replacing pkger with a different library
    Currently removing the test for now, until it can be resolved
    Should put the test back when the issue has been resolved
    
    * Update tests for server
    
    * Update ui ordering
    
    * Attempt to fix up test failure on windows
    Issue with files not being found on windows by SpaHandler
    https://github.com/anz-bank/sysl/pull/639/checks?check_run_id=502574871
    
    Issue has been created to document pkger not working on windows with Sysl UI
    https://github.com/anz-bank/sysl/issues/678
    
    For now, a workaround in the tests have been implemented to mock out the filesystem using an interface
    
    * Fix PR formatting suggestions
    
    Co-authored-by: Jack Zheng <jack.zheng@anz.com>

---
+++ cmd/sysl/cmd_ui.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/anz-bank/sysl/pkg/cmdutils"
+	"github.com/anz-bank/sysl/pkg/ui"
+	"github.com/sirupsen/logrus"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var uiFields = `
+team,
+team.slack,
+owner.name,
+owner.email,
+file.version,
+release.version,
+description,
+deploy.env1.url,
+deploy.sit1.url,
+deploy.sit2.url,
+deploy.qa.url,
+deploy.prod.url,
+repo.url,
+docs.url,
+type`
+
+type uiCmd struct {
+	host   string
+	fields string
+	grpcui bool
+}
+
+func (p *uiCmd) Name() string       { return "ui" }
+func (p *uiCmd) MaxSyslModule() int { return 1 }
+
+func (p *uiCmd) Configure(app *kingpin.Application) *kingpin.CmdClause {
+	cmd := app.Command(p.Name(), "Starts the Sysl UI which displays a visual view of apps defined in Sysl.")
+	cmd.Flag("host", "host and port to serve on").Default(":8080").Short('h').StringVar(&p.host)
+	cmd.Flag("fields", "fields to display on the UI, separated by comma").Default(uiFields).Short('f').StringVar(&p.fields) //nolint:lll
+	cmd.Flag("grpcui", "enables the grpcUI handlers").BoolVar(&p.grpcui)
+	return cmd
+}
+
+func (p *uiCmd) Execute(args cmdutils.ExecuteArgs) error {
+	args.Logger.Debugf("ui: %+v", *p)
+	syslUI := ui.SyslUI{
+		Host:    p.host,
+		Fields:  strings.Split(p.fields, ","),
+		Fs:      args.Filesystem,
+		Log:     args.Logger,
+		Modules: args.Modules,
+		GRPCUI:  p.grpcui,
+	}
+	args.Logger.SetLevel(logrus.InfoLevel)
+	server, err := syslUI.GenerateServer()
+	if err != nil {
+		return err
+	}
+	err = server.Setup()
+	if err != nil {
+		return err
+	}
+	return server.Serve()
+}
