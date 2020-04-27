8e33be091d87d33908ef5b8576d4c12554fc14fc
ashwinsajiv <37055705+ashwinsajiv@users.noreply.github.com>
2020-04-27 15:21:07 +1000

    Endpoint analysis diagram in mermaidjs (#803)
    * Endpoint analysis diagram in mermaidjs
    
    - Also removed old mermaid directory

---
+++ pkg/mermaid/endpointanalysisdiagram/endpointanalysisdiagram_test.go
@@ -0,0 +1,58 @@
+package endpointanalysisdiagram
+
+import (
+	"testing"
+
+	"github.com/alecthomas/assert"
+	"github.com/anz-bank/sysl/pkg/parse"
+	"github.com/anz-bank/sysl/pkg/syslutil"
+	"github.com/spf13/afero"
+)
+
+func TestGenerateMermaidIntegrationDiagram(t *testing.T) {
+	m, err := parse.NewParser().Parse("demo/simple/sysl-sd.sysl",
+		syslutil.NewChrootFs(afero.NewOsFs(), projectDir))
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := GenerateEndpointAnalysisDiagram(m)
+	assert.NotNil(t, m)
+	assert.NotNil(t, r)
+	assert.Nil(t, err)
+}
+
+func TestGenerateMermaidIntegrationDiagram1(t *testing.T) {
+	m, err := parse.NewParser().Parse("demo/simple/sysl-sd2.sysl",
+		syslutil.NewChrootFs(afero.NewOsFs(), projectDir))
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := GenerateEndpointAnalysisDiagram(m)
+	assert.NotNil(t, m)
+	assert.NotNil(t, r)
+	assert.Nil(t, err)
+}
+
+func TestGenerateMermaidIntegrationDiagram2(t *testing.T) {
+	m, err := parse.NewParser().Parse("demo/simple/best-ever-sysl-example.sysl",
+		syslutil.NewChrootFs(afero.NewOsFs(), projectDir))
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := GenerateEndpointAnalysisDiagram(m)
+	assert.NotNil(t, m)
+	assert.NotNil(t, r)
+	assert.Nil(t, err)
+}
+
+func TestGenerateMermaidIntegrationDiagram3(t *testing.T) {
+	m, err := parse.NewParser().Parse("demo/simple/sysl-app-hyperlink.sysl",
+		syslutil.NewChrootFs(afero.NewOsFs(), projectDir))
+	if err != nil {
+		t.Error(err)
+	}
+	r, err := GenerateEndpointAnalysisDiagram(m)
+	assert.NotNil(t, m)
+	assert.NotNil(t, r)
+	assert.Nil(t, err)
+}
