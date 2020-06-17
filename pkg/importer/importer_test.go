5a77d917c4ccad22d4256d6338db63c9181614bc
Jack Zheng <jack.zheng@anz.com>
2020-06-17 17:42:29 +1000

    Refactor tests to use Importer interface

--- pkg/importer/importer_test.go
+++ pkg/importer/importer_test.go
@@ -17,8 +17,6 @@ type testConfig struct {
 	name          string
 	testDir       string
 	testExtension string
-	mode          string
-	fn            Func
 }
 
 func runImportEqualityTests(t *testing.T, cfg testConfig) {
@@ -33,23 +31,21 @@ func runImportEqualityTests(t *testing.T, cfg testConfig) {
 		ext := filepath.Ext(f.Name())
 		if strings.EqualFold(ext, cfg.testExtension) {
 			filename := strings.TrimSuffix(f.Name(), ext)
-			t.Run(fmt.Sprintf("%s - %s", cfg.name, filename), func(t *testing.T) {
+			t.Run(fmt.Sprintf("%s-%s", cfg.name, filename), func(t *testing.T) {
 				t.Parallel()
-				swaggerFile := syslutil.MustAbsolute(t, filepath.Join(cfg.testDir, filename+cfg.testExtension))
-				input, err := ioutil.ReadFile(swaggerFile)
+				fileToImport := syslutil.MustAbsolute(t, filepath.Join(cfg.testDir, filename+cfg.testExtension))
+				input, err := ioutil.ReadFile(fileToImport)
 				require.NoError(t, err)
 				syslFile := filepath.Join(cfg.testDir, filename+".sysl")
 				expected, err := ioutil.ReadFile(syslFile)
 				require.NoError(t, err)
 				expected = syslutil.HandleCRLF(expected)
-
-				outputData := OutputData{
-					AppName:     "testapp",
-					Package:     "package_foo",
-					SwaggerRoot: filepath.Dir(swaggerFile),
-					Mode:        cfg.mode,
-				}
-				result, err := cfg.fn(outputData, string(input), logger)
+				absFilePath, err := filepath.Abs(filepath.Join(cfg.testDir, filename+cfg.testExtension))
+				require.NoError(t, err)
+				imp, err := Factory(absFilePath, input, logger)
+				require.NoError(t, err)
+				imp.WithAppName("testapp").WithPackage("package_foo")
+				result, err := imp.Load(string(input))
 				require.NoError(t, err)
 				require.Equal(t, string(expected), result)
 			})
@@ -62,18 +58,14 @@ func TestLoadSwaggerFromTestFiles(t *testing.T) {
 		name:          "TestLoadSwaggerFromTestFiles",
 		testDir:       "tests-swagger",
 		testExtension: ".yaml",
-		mode:          ModeSwagger,
-		fn:            LoadSwaggerText,
 	})
 }
 
-func TestLoadOpenApiFromTestFiles(t *testing.T) {
+func TestLoadOpenAPIFromTestFiles(t *testing.T) {
 	runImportEqualityTests(t, testConfig{
 		name:          "TestLoadOpenAPIFromTestFiles",
 		testDir:       "tests-openapi",
 		testExtension: ".yaml",
-		mode:          ModeOpenAPI,
-		fn:            LoadOpenAPIText,
 	})
 }
 
@@ -82,8 +74,6 @@ func TestLoadXSDFromTestFiles(t *testing.T) {
 		name:          "TestLoadXSDFromTestFiles",
 		testDir:       "tests-xsd",
 		testExtension: ".xsd",
-		mode:          ModeXSD,
-		fn:            LoadXSDText,
 	})
 }
 
