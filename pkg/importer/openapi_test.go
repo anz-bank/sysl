cb8a2b1d6fcf15e8597b3b24582b234a2956f2fc
Jack Zheng <jack.zheng@anz.com>
2020-06-19 12:29:26 +1000

    Fix endpoint name escaping for responses
    Fixes an issue where the importer maps each response to a separate type, using the endpoint name as a prefix. This endpoint name prefix wasn't getting special characters escaped correctly

--- pkg/importer/openapi_test.go
+++ pkg/importer/openapi_test.go
@@ -55,3 +55,59 @@ components:
 	assert.NoError(t, err)
 	t.Log(result)
 }
+
+func TestLoadOpenAPI3RefSpecialChars(t *testing.T) {
+	spec := `openapi: "3.0"
+info:
+  title: Simple
+paths:
+  /testSomeEndpoint:SpecialChars:
+    get:
+      responses:
+        200:
+          description: "200 OK"
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/SimpleObj:WithSpecialChar"
+        "500":
+          description: "Error"
+          headers:
+            content-type:
+              schema:
+                type: string
+                example: "application/json"
+            request-id:
+              schema:
+                type: string
+                example: "12345678-1234-1234-1234-123456789012"
+          content:
+              application/json:
+                schema:
+                  $ref: "#/components/schemas/SimpleObj:WithSpecialChar"
+
+components:
+  schemas:
+    SimpleObj:WithSpecialChar:
+      type: object
+      properties:
+        name:
+          type: string
+    SimpleObj2:
+      type: object
+      properties:
+        name:
+          type: SimpleObj
+  responses:
+    500Response:
+      description: Internal Server Error
+      schema:
+        $ref: "#/components/schemas/SimpleObj:WithSpecialChar"
+`
+	logger := logrus.New()
+	basePath := ""
+	importer := MakeOpenAPI3Importer(logger, basePath, "")
+	result, err := importer.Load(spec)
+	assert.NoError(t, err)
+	t.Log(result)
+}
