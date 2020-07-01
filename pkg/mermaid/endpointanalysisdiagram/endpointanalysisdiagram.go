aa0b2376b26937e68c9467a2b41e8c6066c82d69
Ashwin Sajiv <37055705+ashwinsajiv@users.noreply.github.com>
2020-07-01 14:03:01 +1000

    Update pkg/mermaid/endpointanalysisdiagram/endpointanalysisdiagram.go
    Co-authored-by: Jack Zheng <j.zheng@live.com.au>

--- pkg/mermaid/endpointanalysisdiagram/endpointanalysisdiagram.go
+++ pkg/mermaid/endpointanalysisdiagram/endpointanalysisdiagram.go
@@ -18,7 +18,7 @@ func GenerateEndpointAnalysisDiagram(m *sysl.Module) (string, error) {
 	return generateEndpointAnalysisDiagramHelper(m, &[]externalLink{}, true)
 }
 
-//Similar to the above, but accepts a list of applications and returns a diagram specific to those
+//Similar to the above, but accepts a slice of application names and returns a diagram only including applications specified
 func GenerateMultipleAppEndpointAnalysisDiagram(m *sysl.Module, appNames []string) (string, error) {
 	return generateMultipleAppEndpointAnalysisDiagramHelper(m, appNames, &[]externalLink{}, true)
 }
