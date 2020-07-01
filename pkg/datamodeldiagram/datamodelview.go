29e31b07d1efa63802ee805e0ab843056c5d2a17
Chloe <cikeyplanet@gmail.com>
2020-07-01 11:52:23 +1000

    fix test

--- pkg/datamodeldiagram/datamodelview.go
+++ pkg/datamodeldiagram/datamodelview.go
@@ -166,7 +166,7 @@ func (v *DataModelView) DrawPrimitive(
 ) {
 	entityTokens := strings.Split(viewParam.EntityName, ".")
 	encEntity := v.UniqueVarForAppName(entityTokens[len(entityTokens)-1])
-	v.StringBuilder.WriteString(fmt.Sprintf("%s \"%s\" as %s %s(%s,%s)%s%s {\n", classString, viewParam.EntityName,
+	v.StringBuilder.WriteString(fmt.Sprintf("%s \"%s\" as %s %s(%s,%s) %s%s {\n", classString, viewParam.EntityName,
 		encEntity, entityLessThanArrow, viewParam.EntityHeader, viewParam.EntityColor, strings.ToLower(entity), entityGreaterThanArrow))
 
 	if _, exists := relationshipMap[encEntity]; !exists {
