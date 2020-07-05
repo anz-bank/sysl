1760e3e42da4bc17c7fb7bee876b8c79053c5315
Jack Zheng <jack.zheng@anz.com>
2020-07-05 20:51:09 +1000

    Rename function due to revert
    Renames getSyslSafeName to GetSyslSafeEndpoint as it has been renamed due to the revert

--- pkg/importer/writer.go
+++ pkg/importer/writer.go
@@ -292,7 +292,7 @@ func (w *writer) writeDefinition(t *StandardType) {
 		}
 		suffix = appendAttributesString(suffix, prop.Attributes)
 
-		name := getSyslSafeName(prop.Name)
+		name := getSyslSafeEndpoint(prop.Name)
 		if IsBuiltIn(name) {
 			name += "_"
 		}
