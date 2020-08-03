aebc2c8b01f76799606743009eee97c60bec7da5
Jack Zheng <jack.zheng@anz.com>
2020-08-03 15:06:21 +1000

    Unescape first

--- pkg/mermaid/utils.go
+++ pkg/mermaid/utils.go
@@ -11,6 +11,7 @@ const ProjectDir = "../../../"
 
 //cleanString replaces certain characters in the string suitable for mermaid
 func CleanString(temp string) string {
+	temp = parse.MustUnescape(temp)
 	temp = strings.ReplaceAll(temp, " ", "")
 	temp = strings.ReplaceAll(temp, "{", "_")
 	temp = strings.ReplaceAll(temp, "}", "_")
@@ -20,5 +21,5 @@ func CleanString(temp string) string {
 	temp = strings.ReplaceAll(temp, "~", "")
 	temp = strings.ReplaceAll(temp, ":", "_")
 	temp = strings.ReplaceAll(temp, "<", "")
-	return parse.MustUnescape(temp)
+	return temp
 }
