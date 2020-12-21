383c347284a9bbdcad5e405a6065a3e3e32b3aeb
Novan Allanadi <25704935+nofun97@users.noreply.github.com>
2020-12-21 22:02:10 +1100

    fix return type in relational model statements (#392)
    * fix return type in relational model statements

--- pkg/arrai/out/terraform_gdc.tf
+++ pkg/arrai/out/terraform_gdc.tf
@@ -1,58 +1,21 @@
-resource "google_data_catalog_entry" "foo_entry" {
-  entry_group = google_data_catalog_entry_group.entry_group.id
-  entry_id = "Source.Foo"
-
-  user_specified_type = "Foo"
-  user_specified_system = "Source"
-  linked_resource = "dev/sysl/pkg/arrai/model.sysl:6"
-
-  display_name = "Foo"
-  description  = "A database.
-   Stores data."
-
-  schema = <<EOF
-{
-  "columns": [
-    {
-          "column": "y",
-          "description": "A Foo.
-           Represents foo things.",
-          "mode": "REQUIRED",
-          "type": "INT"
-        },
-    {
-          "column": "x",
-          "description": "The x value.",
-          "mode": "REQUIRED",
-          "type": "INT"
-        }
-  ]
-}
-EOF
-}
-
-resource "google_data_catalog_entry_group" "entry_group" {
-  entry_group_id = "Source_group"
-}
-
 resource "google_data_catalog_entry" "bar_entry" {
   entry_group = google_data_catalog_entry_group.entry_group.id
   entry_id = "Source.Bar"
 
   user_specified_type = "Bar"
   user_specified_system = "Source"
-  linked_resource = "dev/sysl/pkg/arrai/model.sysl:14"
+  linked_resource = "model.sysl:14"
 
   display_name = "Bar"
   description  = "A database.
-   Stores data."
+ Stores data."
 
   schema = <<EOF
 {
   "columns": [
     {
           "column": "a",
-          "description": "A bar table.",
+          "description": "",
           "mode": "REQUIRED",
           "type": "STRING"
         },
@@ -66,6 +29,42 @@ resource "google_data_catalog_entry" "bar_entry" {
           "column": "b",
           "description": "An optional int",
           "mode": "NULLABLE",
+          "type": "INT"
+        }
+  ]
+}
+EOF
+}
+
+resource "google_data_catalog_entry_group" "entry_group" {
+  entry_group_id = "Source_group"
+}
+
+resource "google_data_catalog_entry" "foo_entry" {
+  entry_group = google_data_catalog_entry_group.entry_group.id
+  entry_id = "Source.Foo"
+
+  user_specified_type = "Foo"
+  user_specified_system = "Source"
+  linked_resource = "model.sysl:6"
+
+  display_name = "Foo"
+  description  = "A database.
+ Stores data."
+
+  schema = <<EOF
+{
+  "columns": [
+    {
+          "column": "y",
+          "description": "",
+          "mode": "REQUIRED",
+          "type": "INT"
+        },
+    {
+          "column": "x",
+          "description": "The x value.",
+          "mode": "REQUIRED",
           "type": "INT"
         }
   ]
