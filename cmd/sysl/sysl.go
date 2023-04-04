anz-bank/sysl

commit 60600a460fe9e8403b3935861b1b72457fb7523c

Embed arr.ai into Sysl (#552)
Changes proposed in this pull request:
- Add `transform` command to allow running sysl transforms written in arr.ai.
- Allowed implementing importers using embedded arr.ai transforms.

Breaking changes:
- The `sysl import` command no longer defaults to outputting to `output.sysl` but instead outputs to stdout if no output flag was specified.
- The `relmod.NormalizeSpec` public API was removed to solve a cyclic dependency. No apparent users. Use `relmod.Normalize` instead.

cmd/sysl/sysl.go
@@ -8,7 +8,6 @@ import (
 	"github.com/anz-bank/sysl/pkg/arrai"
 
 	"github.com/anz-bank/sysl/pkg/cfg"
-	"github.com/anz-bank/sysl/pkg/parse"
 	"github.com/anz-bank/sysl/pkg/syslutil"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -92,12 +91,22 @@ func main2(
 			logger.Debugln(err)
 			logger.Errorln(arraiErr.ShortMsg)
 		} else {
-			logger.Errorln(err.Error())
+			var exitCode = 1
+			if err, ok := err.(syslutil.Exit); ok {
+				exitCode = err.Code
+			}
+
+			switch exitCode {
+			case 0:
+				logger.Infoln(err.Error())
+			case 2:
+				logger.Warnln(err.Error())
+			default:
+				logger.Errorln(err.Error())
+			}
+
+			return exitCode
 		}
-		if err, ok := err.(parse.Exit); ok {
-			return err.Code
-		}
-		return 1
 	}
 	return 0
 }

.github/workflows/go.yml
@@ -18,7 +18,7 @@ jobs:
       - name: Set up Go
         uses: actions/setup-go@v2
         with:
-          go-version: 1.13
+          go-version: 1.16
 
       - uses: actions/cache@v2
         with:
@@ -62,7 +62,7 @@ jobs:
     - name: Set up Go
       uses: actions/setup-go@v2
       with:
-        go-version: 1.13
+        go-version: 1.16
 
     - uses: actions/cache@v2
       with:
@@ -92,7 +92,7 @@ jobs:
     - name: Set up Go
       uses: actions/setup-go@v2
       with:
-        go-version: 1.13
+        go-version: 1.16
 
     - uses: actions/cache@v2
       with:
@@ -144,10 +144,10 @@ jobs:
     name: Builds the sysl docker image
     runs-on: ubuntu-latest
     steps:
-      - name: Set up Go 1.13
+      - name: Set up Go 1.16
         uses: actions/setup-go@v2
         with:
-          go-version: 1.13
+          go-version: 1.16
 
       - name: Install go-bindata
         run: go get github.com/anz-bank/go-bindata/...
@@ -177,13 +177,13 @@ jobs:
       - name: Set up Go
         uses: actions/setup-go@v2
         with:
-          go-version: 1.13
+          go-version: 1.16
 
       - name: Check out code
         uses: actions/checkout@v2
 
       - name: Install go-bindata
-        run: go get github.com/anz-bank/go-bindata/... && go mod tidy
+        run: go get github.com/anz-bank/go-bindata/...
 
       - name: Generate intermediate files
         run: make generate

.github/workflows/release.yml
@@ -14,10 +14,10 @@ jobs:
     name: Release Sysl
     runs-on: ubuntu-latest
     steps:
-      - name: Set up Go 1.13
+      - name: Set up Go 1.16
         uses: actions/setup-go@v2
         with:
-          go-version: 1.13
+          go-version: 1.16
 
       - name: Checkout
         uses: actions/checkout@v2

.golangci.yml
@@ -22,16 +22,15 @@ linters:
     - gocyclo
     - gofmt
     - goimports
-    - golint
+    - revive
     - gosec
     - gosimple
     - govet
     - ineffassign
-    - interfacer
     - lll
     - misspell
     - nakedret
-    - scopelint
+    - exportloopref
     - staticcheck
     - structcheck
     - stylecheck

Dockerfile
@@ -4,8 +4,8 @@
 #
 # The produced image is published to https://hub.docker.com/r/anzbank/sysl
 
-ARG go_ver=1.13.8
-ARG alpine_ver=3.11
+ARG go_ver=1.16.6
+ARG alpine_ver=3.14
 
 FROM golang:${go_ver}-alpine${alpine_ver} as builder
 

Makefile
@@ -25,7 +25,7 @@ lint-docker: generate
 		-e GOCACHE=/cache/go \
 		-v ${GOPATH}/pkg:/go/pkg \
 		-w /app \
-		golangci/golangci-lint:v1.30.0 \
+		golangci/golangci-lint:v1.41.1 \
 			golangci-lint run -v
 
 tidy:
@@ -44,7 +44,7 @@ test-arrai:
 	$(ARRAI) test
 
 coverage: generate
-	./scripts/test-with-coverage.sh 80
+	./scripts/test-with-coverage.sh 75
 
 # Updates golden test files in pkg/parse.
 golden: generate

cmd/sysl/cmd_export.go
@@ -103,7 +103,7 @@ func (p *exportCmd) determineOperationMode(filename string) (string, error) {
 	case yamlMode:
 		return yamlMode, nil
 	default:
-		return "", fmt.Errorf("invalid output file format %s", fileExtn)
+		return "", fmt.Errorf("could not determine output format from specified output file extension '%s'", fileExtn)
 	}
 }
 

cmd/sysl/cmd_import.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"sort"
@@ -19,8 +18,8 @@ import (
 type importCmd struct {
 	importer.OutputData
 	filename string
-	outfile  string
 	format   string
+	outFile  string
 }
 
 func (p *importCmd) Name() string       { return "import" }
@@ -29,36 +28,34 @@ func (p *importCmd) MaxSyslModule() int { return 0 }
 func (p *importCmd) Configure(app *kingpin.Application) *kingpin.CmdClause {
 	optsText := buildOptionsText(importer.Formats)
 	cmd := app.Command(p.Name(), "Import foreign type to Sysl. Supported types: ["+optsText+"]")
-	cmd.Flag("input", "input filename").Short('i').Required().StringVar(&p.filename)
+	cmd.Flag("input", "path of file to import").Short('i').Required().StringVar(&p.filename)
 	cmd.Flag("app-name",
 		"name of the Sysl app to define in Sysl model.").Required().Short('a').StringVar(&p.AppName)
 	cmd.Flag("package",
 		"name of the Sysl package to define in Sysl model.").Short('p').StringVar(&p.Package)
-	cmd.Flag("output", "output filename").Default("output.sysl").Short('o').StringVar(&p.outfile)
-	cmd.Flag("format", fmt.Sprintf("format of the input filename, options: [%s]."+
-		"Formats are autodetected, but this can force the use of a particular importer.", optsText)).StringVar(&p.format)
+	cmd.Flag("output", "path of file to write the imported sysl, writes to stdout when not specified").
+		Short('o').StringVar(&p.outFile)
+	cmd.Flag("format", fmt.Sprintf("format of the input filename, options: [%s]. "+
+		"Formats are autodetected, but this can force the use of a particular importer.", optsText)).Short('f').
+		StringVar(&p.format)
 	return cmd
 }
 
 func (p *importCmd) Execute(args cmdutils.ExecuteArgs) error {
-	info, err := os.Stat(p.filename)
-	if os.IsNotExist(err) {
+	content, err := afero.ReadFile(args.Filesystem, p.filename)
+	if err != nil {
 		return err
 	}
-	var data []byte
-	if !info.IsDir() {
-		data, err = ioutil.ReadFile(p.filename)
-		if err != nil {
-			return err
-		}
-	}
-
 	var imp importer.Importer
 	inputFilePath, err := filepath.Abs(p.filename)
 	if err != nil {
 		return err
 	}
-	imp, err = importer.Factory(inputFilePath, info.IsDir(), p.format, data, logrus.New())
+	isDir, err := afero.IsDir(args.Filesystem, p.filename)
+	if err != nil {
+		return err
+	}
+	imp, err = importer.Factory(inputFilePath, isDir, p.format, content, logrus.New())
 	if err != nil {
 		return err
 	}
@@ -70,13 +67,17 @@ func (p *importCmd) Execute(args cmdutils.ExecuteArgs) error {
 	case *importer.ArraiImporter:
 		output, err = imp.LoadFile(inputFilePath)
 	default:
-		output, err = imp.Load(string(data))
+		output, err = imp.Load(string(content))
 	}
 	if err != nil {
 		return err
 	}
 
-	return afero.WriteFile(args.Filesystem, p.outfile, []byte(output), os.ModePerm)
+	if p.outFile != "" {
+		return afero.WriteFile(args.Filesystem, p.outFile, []byte(output), os.ModePerm)
+	}
+	_, err = fmt.Println(output)
+	return err
 }
 
 func buildOptionsText(opts []importer.Format) string {

commit 97059cba627d513645f9b9c898a0a11aa6a83696

Fix exit code when an importer fails (#797)
This was accidentally broken in in #552

cmd/sysl/sysl.go
@@ -86,11 +86,11 @@ func main2(
 ) int {
 	if err := main3(args, fs, logger, stdin); err != nil {
 		arraiErr, ok := errors.Cause(err).(arrai.ExecutionError)
+		var exitCode = 1
 		if ok {
 			logger.Debugln(err)
 			logger.Errorln(arraiErr.ShortMsg)
 		} else {
-			var exitCode = 1
 			if err, ok := err.(syslutil.Exit); ok {
 				exitCode = err.Code
 			}
@@ -103,9 +103,8 @@ func main2(
 			default:
 				logger.Errorln(err.Error())
 			}
-
-			return exitCode
 		}
+		return exitCode
 	}
 	return 0
 }